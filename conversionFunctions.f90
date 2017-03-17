7473a5d1c6c9594e10e6677c5d75dd9d2603c44c
Cox, Samuel <sc676@leicester.ac.uk>
2017-03-17 16:48:10 +0000

    Run updated tools/fix_style.py on conversionFunctions.f90 to catch SUBROUTINE lines.

--- conversionFunctions.f90
+++ conversionFunctions.f90
@@ -1,7 +1,7 @@
-SUBROUTINE convertRHtoConcH2O(h2o,temp,rh)
-  DOUBLE PRECISION h2o, temp, rh, exponent,e1
+SUBROUTINE convertRHtoConcH2O (h2o, temp, rh)
+  DOUBLE PRECISION h2o, temp, rh, exponent, e1
 
-  exponent = EXP(-1.00d00*(597.30d00-0.57d00*(temp - 273.16d00))* &
+  exponent = EXP (-1.00d00*(597.30d00-0.57d00*(temp - 273.16d00))* &
        18.00d00/1.986d00*(1.00d00/temp - 1.00d00/273.16d00))
 
   e1 = 10d0/(1.38D-16*TEMP)*RH

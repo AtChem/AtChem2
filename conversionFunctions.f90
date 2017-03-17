be42667e9fe625f7b4840adf1983c1662def0751
Cox, Samuel <sc676@leicester.ac.uk>
2017-03-17 16:41:52 +0000

    Tidy up of conversionFunctions.f90 by using atom-beautify and tools/fix_style.py, plussing manually checking for incorrect multiple spaces with a regex find, and manual equals sign checking.

--- conversionFunctions.f90
+++ conversionFunctions.f90
@@ -1,10 +1,10 @@
-subroutine convertRHtoConcH2O(H2o,temp,RH)
-    double precision h2o, temp, rh, exponen,e1
+SUBROUTINE convertRHtoConcH2O(h2o,temp,rh)
+  DOUBLE PRECISION h2o, temp, rh, exponent,e1
 
-    exponen = exp(-1.00d00*(597.30d00-0.57d00*(temp - 273.16d00))* &
-  18.00d00/1.986d00*(1.00d00/temp - 1.00d00/273.16d00))
-  
-    e1 = 10d0/(1.38D-16*TEMP)*RH
-    h2o = 6.1078d0*exponen*e1
-    return
-end
+  exponent = EXP(-1.00d00*(597.30d00-0.57d00*(temp - 273.16d00))* &
+       18.00d00/1.986d00*(1.00d00/temp - 1.00d00/273.16d00))
+
+  e1 = 10d0/(1.38D-16*TEMP)*RH
+  h2o = 6.1078d0*exponent*e1
+  RETURN
+END SUBROUTINE convertRHtoConcH2O

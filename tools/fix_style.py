0208737e25aa89cb0e3c48dbe4cdfbe3ecb58395
Cox, Samuel <sc676@leicester.ac.uk>
2017-03-20 12:43:05 +0000

    Update tools/fix_style.py to split some of the nested calls.

--- tools/fix_style.py
+++ tools/fix_style.py
@@ -8,43 +8,52 @@ with open(filename, 'r') as input_file:
     lines = input_file.readlines()
 
 with open(filename, 'w') as output_file:
-    #print lines
+
     outputs = []
     for line in lines:
         # Replacements happen in reverse order on this line:
-        # A comma followed by any letter, digit, ( ' or - should have a space trailing.
-        to_output = re.sub(",(?=[a-zA-Z0-9('-])", ', ',
-                           # Opening bracket preceded by an uppercase letter or a number should have a space in front.
-                           re.sub('(?<=[A-Z0-9])\(', ' (',
-                                  # Replace .LT. etc with lowercase and with no whitespace. This and the next 3 are all case-insensitive
-                                  re.sub('\s*\.LT\.\s*', '.LT.',
-                                  re.sub('\s*\.LE\.\s*', '.LE.',
-                                        re.sub('\s*\.GT\.\s*', '.GT.',
-                                        re.sub('\s*\.GE\.\s*', '.GE.',
-                                               re.sub('\s*\.EQ\.\s*', '.EQ.',
+        # Any ending bracket followed by whitespace should be followed by exactly one space
+        to_output = re.sub('\)[ \t]+', ') ',
+               # Any ending bracket should be followed by exactly one space if it's to be followed by a letter, digit, or single-quote
+               re.sub("\)(?=[\w\d'])", ') ',
+                      # Any ending bracket followed by a double-quote, should have a single space.
+                      re.sub('\)(?=")', ') ',
+                             # Place all :: with exactly one space either side.
+                             re.sub('\s*::\s*', ' :: ',
+                                    # Replace e.g. ( Len =  by (LEN=
+                                    re.sub('\(\s*LEN\s*=\s*', '(LEN=',
+                                           line, flags=re.IGNORECASE
+                                           )
+                                    )
+                             )
+                      ),
+               )
+
+        # Replace .LT. etc with lowercase and with no whitespace. These are all case-insensitive
+        to_output = re.sub('\s*\.LT\.\s*', '.LT.',
+                           re.sub('\s*\.LE\.\s*', '.LE.',
+                                  re.sub('\s*\.GT\.\s*', '.GT.',
+                                         re.sub('\s*\.GE\.\s*', '.GE.',
+                                                re.sub('\s*\.EQ\.\s*', '.EQ.',
                                                        re.sub('\s*\.NE\.\s*', '.NE.',
-                                                              # Any ending bracket followed by whitespace should be followed by exactly one space
-                                                              re.sub('\)[ \t]+', ') ',
-                                                                     # Any ending bracket should be followed by exactly one space if it's to be followed by a letter, digit, or single-quote
-                                                                     re.sub("\)(?=[\w\d'])", ') ',
-                                                                            # Any ending bracket followed by a double-quote, should have a single space.
-                                                                            re.sub('\)(?=")', ') ',
-                                                                                  # Place all :: with exactly one space either side.
-                                                                                   re.sub('\s*::\s*', ' :: ',
-                                                                                          # Replace e.g. ( Len =  by (LEN=
-                                                                                          re.sub('\(\s*LEN\s*=\s*', '(LEN=',
-                                                                                                 line, flags=re.IGNORECASE
-                                                                                                )
-                                                                                          )
-                                                                                  )
-                                                                            ),
-                                                                     ),
+                                                              to_output,
                                                               flags=re.IGNORECASE),
                                                        flags=re.IGNORECASE),
-                                               flags=re.IGNORECASE),
-                                               flags=re.IGNORECASE),
+                                                flags=re.IGNORECASE),
                                          flags=re.IGNORECASE),
-                                         flags=re.IGNORECASE),
+                                  flags=re.IGNORECASE),
+                           flags=re.IGNORECASE)
+
+        # A comma followed by any letter, digit, ( ' or - should have a space trailing.
+        to_output = re.sub(",(?=[a-zA-Z0-9('-])", ', ',
+                           # LOG10 and EXP are math functions so should be not followed by a space
+                           re.sub('LOG10 \(', 'LOG10(',
+                                  re.sub('EXP \(', 'EXP(',
+                                         # Opening bracket preceded by an uppercase letter or a number should have a space in front.
+                                         re.sub('(?<=[A-Z0-9])\(', ' (',
+                                                to_output
+                                                )
+                                         )
                                   )
                            )
         # If it's a CALL line, then make the first opening bracket be preceded by exactly one space

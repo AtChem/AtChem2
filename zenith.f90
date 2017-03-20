b203aca8e33d5dd9d94c7bf965fa3699b22fd4a1
Cox, Samuel <sc676@leicester.ac.uk>
2017-03-20 13:50:23 +0000

    Tidy up of zenith.f90 by using atom-beautify and tools/fix_style.py, plus manually checking for incorrect multiple spaces with a regex find, and manual equals sign checking.

--- zenith.f90
+++ zenith.f90
@@ -1,27 +1,27 @@
-SUBROUTINE ZENITH(THETA, SECX, COSX, TTIME,DEC)
-    USE zenithData
-    USE SZACalcVars
-    double precision PI,RADIAN,DEC,TTIME,THETA, COSX,SECX
-    double precision rampValue
-    ! DOUBLE PRECISION::,sinld,cosld,LAT,LHA,longt
-    ! SOLAR DECLINATION ANGLE FROM JULY 1ST - HARWELL TRAJ MODEL
+SUBROUTINE ZENITH (THETA, SECX, COSX, TTIME, DEC)
+  USE zenithData
+  USE SZACalcVars
+  DOUBLE PRECISION PI, RADIAN, DEC, TTIME, THETA, COSX, SECX
+  DOUBLE PRECISION rampValue
+  ! DOUBLE PRECISION :: , sinld, cosld, LAT, LHA, longt
+  ! SOLAR DECLINATION ANGLE FROM JULY 1ST - HARWELL TRAJ MODEL
 
-    lat = latitude
-    longt= longitude
-    PI = 4.0*ATAN(1.0)
-    ! LOCAL HOUR ANGLE - REPRESENTING TIME OF DAY
-    lha = (1 + ((TTIME ) / 4.32D+04)) * PI
-    RADIAN = 180.0/PI
-    LAT = LAT/RADIAN
-    ! DEC = DEC/RADIAN
-    THETA = ACOS(COS(LHA)*COS(DEC)*COS(LAT)+SIN(DEC)*SIN(LAT)+1.0D-30)
-    sinld = sin(lat)*sin(dec)
-    cosld = cos(lat)*cos(dec)
+  lat = latitude
+  longt= longitude
+  PI = 4.0*ATAN (1.0)
+  ! LOCAL HOUR ANGLE - REPRESENTING TIME OF DAY
+  lha = (1 + ((TTIME ) / 4.32D+04)) * PI
+  RADIAN = 180.0/PI
+  LAT = LAT/RADIAN
+  ! DEC = DEC/RADIAN
+  THETA = ACOS (COS (LHA)*COS (DEC)*COS (LAT)+SIN (DEC)*SIN (LAT)+1.0D-30)
+  sinld = SIN (lat)*SIN (dec)
+  cosld = COS (lat)*COS (dec)
 
-    call  ramp(cos(lha)*cosld+sinld, rampValue)
-    SECX = 1/ (rampValue + 1.00d-30)
+  CALL ramp (COS (lha)*cosld+sinld, rampValue)
+  SECX = 1/ (rampValue + 1.00d-30)
 
-    COSX = (COS(LHA)*COSLD) + SINLD
+  COSX = (COS (LHA)*COSLD) + SINLD
 
-    RETURN
-END
+  RETURN
+END SUBROUTINE ZENITH

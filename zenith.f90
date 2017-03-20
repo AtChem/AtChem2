5aa276c36a16ab5ff029d94fcf03c4717f16e8b0
Cox, Samuel <sc676@leicester.ac.uk>
2017-03-20 14:08:04 +0000

    Standardise variables as camelcase in zenith.f90.

--- zenith.f90
+++ zenith.f90
@@ -1,27 +1,27 @@
-SUBROUTINE ZENITH (THETA, SECX, COSX, TTIME, DEC)
+SUBROUTINE zenith (theta, secx, cosx, ttime, dec)
   USE zenithData
   USE SZACalcVars
-  DOUBLE PRECISION PI, RADIAN, DEC, TTIME, THETA, COSX, SECX
+  DOUBLE PRECISION pi, radian, dec, ttime, theta, cosx, secx
   DOUBLE PRECISION rampValue
-  ! DOUBLE PRECISION :: , sinld, cosld, LAT, LHA, longt
-  ! SOLAR DECLINATION ANGLE FROM JULY 1ST - HARWELL TRAJ MODEL
+  ! DOUBLE PRECISION :: , sinld, cosld, lat, lha, longt
+  ! SOLAR decLINATION ANGLE FROM JULY 1ST - HARWELL TRAJ MODEL
 
   lat = latitude
-  longt= longitude
-  PI = 4.0*ATAN (1.0)
+  longt = longitude
+  pi = 4.0*ATAN (1.0)
   ! LOCAL HOUR ANGLE - REPRESENTING TIME OF DAY
-  lha = (1 + ((TTIME ) / 4.32D+04)) * PI
-  RADIAN = 180.0/PI
-  LAT = LAT/RADIAN
-  ! DEC = DEC/RADIAN
-  THETA = ACOS (COS (LHA)*COS (DEC)*COS (LAT)+SIN (DEC)*SIN (LAT)+1.0D-30)
+  lha = (1 + ((ttime) / 4.32D+04)) * pi
+  radian = 180.0/pi
+  lat = lat/radian
+  ! dec = dec/RADIAN
+  theta = ACOS (COS (LHA)*COS (dec)*COS (lat)+SIN (dec)*SIN (lat)+1.0D-30)
   sinld = SIN (lat)*SIN (dec)
   cosld = COS (lat)*COS (dec)
 
   CALL ramp (COS (lha)*cosld+sinld, rampValue)
-  SECX = 1/ (rampValue + 1.00d-30)
+  secx = 1/ (rampValue + 1.00d-30)
 
-  COSX = (COS (LHA)*COSLD) + SINLD
+  cosx = (COS (lha)*cosld) + sinld
 
   RETURN
-END SUBROUTINE ZENITH
+END SUBROUTINE zenith

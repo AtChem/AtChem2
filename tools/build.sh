9b0467f267baa196ad39f29d88fd9177186b99f4
Cox, Samuel <sc676@leicester.ac.uk>
2017-03-21 17:10:45 +0000

    Add tools/build.sh (again?)

---
+++ tools/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# $1 is the input file
+echo $1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "call run_atchem.py"
+python $DIR/tools/run_atchem.py $1
+echo "cd"
+cd $DIR/..
+echo "make"
+make
+echo "run"
+./atchem
+echo "complete"

55ef820e7c643e1f426757b2396ca455bbc2e1f6
Cox, Samuel <sc676@leicester.ac.uk>
2017-03-21 17:15:01 +0000

    Another try.

--- tools/build.sh
+++ tools/build.sh
@@ -3,6 +3,9 @@
 # $1 is the input file
 echo $1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "dir"
+echo $DIR
+echo $DIR/tools/run_atchem.py
 echo "call run_atchem.py"
 python $DIR/tools/run_atchem.py $1
 echo "cd"

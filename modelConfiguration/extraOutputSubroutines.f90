7107f8bb01283819f2d35ffcf3195ed87e813ba9
rs028 <rob.sommariva@gmail.com>
2017-03-24 20:04:46 +0000

    Move atmosphere() and zenith() to temperature.f90, delete modelConfiguration/extraOutputSubroutines.f90

--- modelConfiguration/extraOutputSubroutines.f90
+++
@@ -1,5 +0,0 @@
-SUBROUTINE atmosphere (o2, n2, m)
-  DOUBLE PRECISION :: o2, n2, m
-  o2 = 0.2095*m
-  n2 = 0.7809*m
-END SUBROUTINE atmosphere
--- temperature.f90
+++ temperature.f90
@@ -1,3 +1,9 @@
+SUBROUTINE atmosphere (o2, n2, m)
+  DOUBLE PRECISION :: o2, n2, m
+  o2 = 0.2095*m
+  n2 = 0.7809*m
+END SUBROUTINE atmosphere
+
 SUBROUTINE temperature (temp, h2o, ttime)
   ! SUBROUTINE TO CALCULATE DIURNAL VARIATIONS IN TEMPERATURE
   DOUBLE PRECISION temp, ttime, rh, h2o, sin
@@ -9,3 +15,31 @@ SUBROUTINE temperature (temp, h2o, ttime)
 
   RETURN
 END SUBROUTINE temperature
+
+SUBROUTINE zenith (theta, secx, cosx, ttime, dec)
+  USE zenithData
+  USE SZACalcVars
+  DOUBLE PRECISION pi, radian, dec, ttime, theta, cosx, secx
+  DOUBLE PRECISION rampValue
+  ! DOUBLE PRECISION :: , sinld, cosld, lat, lha, longt
+  ! SOLAR decLINATION ANGLE FROM JULY 1ST - HARWELL TRAJ MODEL
+
+  lat = latitude
+  longt = longitude
+  pi = 4.0*ATAN (1.0)
+  ! LOCAL HOUR ANGLE - REPRESENTING TIME OF DAY
+  lha = (1 + ((ttime) / 4.32D+04)) * pi
+  radian = 180.0/pi
+  lat = lat/radian
+  ! dec = dec/RADIAN
+  theta = ACOS (COS (LHA)*COS (dec)*COS (lat)+SIN (dec)*SIN (lat)+1.0D-30)
+  sinld = SIN (lat)*SIN (dec)
+  cosld = COS (lat)*COS (dec)
+
+  CALL ramp (COS (lha)*cosld+sinld, rampValue)
+  secx = 1/ (rampValue + 1.00d-30)
+
+  cosx = (COS (lha)*cosld) + sinld
+
+  RETURN
+END SUBROUTINE zenith

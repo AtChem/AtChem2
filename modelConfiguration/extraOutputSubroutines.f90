cf60a9e6750b2ec5a72770a00dc91bdd3df4ca38
rs028 <rob.sommariva@gmail.com>
2017-04-03 14:59:14 +0100

    Move ro2sum to outputFunctions.f90

--- modelConfiguration/extraOutputSubroutines.f90
+++ modelConfiguration/extraOutputSubroutines.f90
@@ -1,8 +1,3 @@
-SUBROUTINE ro2sum (ro2, y)
-  DOUBLE PRECISION :: ro2
-  DOUBLE PRECISION, INTENT (in) :: y(*)
-  ro2 = 0.00e+00
-END SUBROUTINE ro2sum
 
 SUBROUTINE atmosphere (o2, n2, m)
   DOUBLE PRECISION :: o2, n2, m

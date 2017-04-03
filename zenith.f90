53e8f56e8442579ab0bf68d98d5a4a074c19f5a0
rs028 <rob.sommariva@gmail.com>
2017-04-03 15:00:36 +0100

    Merge modelConfiguration/extraOutputSubroutines.f90 with zenith.f90

--- zenith.f90
+++ zenith.f90
@@ -1,3 +1,10 @@
+
+SUBROUTINE atmosphere (o2, n2, m)
+  DOUBLE PRECISION :: o2, n2, m
+  o2 = 0.2095*m
+  n2 = 0.7809*m
+END SUBROUTINE atmosphere
+
 SUBROUTINE zenith (theta, secx, cosx, ttime, dec)
   USE zenithData
   USE SZACalcVars

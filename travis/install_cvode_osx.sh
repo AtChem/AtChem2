941c90cb79770e54d0c17a5e3d5e8c06842f8d9f
Cox, Samuel <sc676@leicester.ac.uk>
2017-04-07 12:07:29 +0100

    Remove make clean from travis/install_cvode_[osx/linux].sh.

--- travis/install_cvode_osx.sh
+++ travis/install_cvode_osx.sh
@@ -8,7 +8,6 @@ tar -zxf cvode-2.9.0.tar.gz
 cd cvode-2.9.0/
 mkdir build
 cd build
-make clean
 cmake -DCMAKE_INSTALL_PREFIX=/Users/travis/build/AtChem/AtChem/cvode \
     -DCMAKE_C_COMPILER:FILEPATH=gcc \
     -DCMAKE_Fortran_COMPILER=/usr/local/Cellar/gcc@4.8/4.8.5/bin/gfortran-4.8 \

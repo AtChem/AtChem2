2ea11a81479860f5515a4a1dca2eff49f6d5fa17
Cox, Samuel <sc676@leicester.ac.uk>
2017-04-12 13:01:55 +0100

    Add tools/install/install_cvode_[linux/osx].sh for easier initial setup and install.

---
+++ tools/install/install_cvode_osx.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# This file downloads and installs CVODE 2.9.0 into the directory given by input
+# argument $1. This is dependent on the exisence of a gcc installation.
+# Edit FORT_COMP to point to an existing gfortran or ifort executable.
+# Example usage:
+#   ./install_cvode_linux.sh /path/to/install/directory
+FORT_COMP=/usr/local/Cellar/gcc@4.8/4.8.5/bin/gfortran-4.8
+
+cd $1
+wget http://computation.llnl.gov/projects/sundials/download/cvode-2.9.0.tar.gz
+
+tar -zxf cvode-2.9.0.tar.gz
+cd cvode-2.9.0/
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$1/cvode \
+    -DCMAKE_C_COMPILER:FILEPATH=gcc \
+    -DCMAKE_Fortran_COMPILER=$FORT_COMP \
+    -DLAPACK_ENABLE:BOOL=ON \
+    -DLAPACK_LIBRARIES=/usr/lib/liblapack.dylib:/usr/lib/libblas.dylib \
+    -DFCMIX_ENABLE:BOOL=ON \
+    -DCMAKE_MACOSX_RPATH:BOOL=ON \
+    ..
+make -j8
+make install

802ac8f2bede439766aa214cfb735996210e9263
Cox, Samuel <sc676@leicester.ac.uk>
2017-04-20 08:45:38 +0100

    Tweak comments for matchNameToNumber() and around where it is called.

--- configFunctions.f90
+++ configFunctions.f90
@@ -42,16 +42,22 @@ END SUBROUTINE writeFileHeaders
 
 SUBROUTINE matchNameToNumber (masterSpeciesList, testSpeciesList, testSpeciesListSize, &
                               masterSpeciesListSize, returnArray, returnArraySize)
-  CHARACTER (LEN=10), intent(IN) :: masterSpeciesList(*)
-  CHARACTER (LEN=10), intent(INOUT) :: testSpeciesList(*)
-  INTEGER, intent(IN) :: testSpeciesListSize, masterSpeciesListSize
-  INTEGER, intent(OUT) :: returnArray(*), returnArraySize
+  ! This takes in masterSpeciesList, and checks whether each member of
+  ! testspeciesList is in masterSpeciesList.
+  ! When it finds a match, it adds the number of the line in masterSpeciesList to
+  ! returnArray in the next available space.
+  ! returnArraySize contains the size of returnArray, which is the
+  ! number of times a match was made
+  CHARACTER (LEN=10), intent(in) :: masterSpeciesList(*)
+  CHARACTER (LEN=10), intent(inout) :: testSpeciesList(*)
+  INTEGER, intent(in) :: testSpeciesListSize, masterSpeciesListSize
+  INTEGER, intent(out) :: returnArray(*), returnArraySize
   INTEGER i, j
   LOGICAL match
 
-  returnArraySize = 0 
+  returnArraySize = 0
   ! loop over testSpeciesList, and masterSpeciesList. If a match is made, then append
-  ! returnArray with the number of that species within the masterSpeciesList
+  ! returnArray with the number of the species from testSpeciesList within the masterSpeciesList
   DO i = 1, testSpeciesListSize
      match = .FALSE.
      DO j = 1, masterSpeciesListSize

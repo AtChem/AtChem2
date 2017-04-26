14f03beea7829673771a2f0fdf64067091317f1a
rs028 <rob.sommariva@gmail.com>
2017-04-26 19:19:27 +0100

    Version for osx; add some comments

--- tools/install/install_numdiff_osx.sh
+++ tools/install/install_numdiff_osx.sh
@@ -3,13 +3,13 @@
 # This script downloads and installs NUMDIFF 5.8.1 into the directory given by input
 # argument $1. This is dependent on the existence of a gcc installation.
 #
-# This is the LINUX/UNIX version.
+# This is the macOSX version.
 #
 # Edit FORT_COMP to point to an existing gfortran or ifort executable.
 #
 # Example usage:
 #   ./install_numdiff_linux.sh /path/to/install/directory
-FORT_COMP=/usr/bin/gfortran
+FORT_COMP=/usr/local/Cellar/gcc@4.8/4.8.5/bin/gfortran-4.8
 
 cd $1
 wget http://ftp.igh.cnrs.fr/pub/nongnu/numdiff/numdiff-5.8.1.tar.gz

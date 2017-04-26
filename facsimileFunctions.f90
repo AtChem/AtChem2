465c177b31a13de12cdfca33fcce1a6701d1dfd4
Cox, Samuel <sc676@leicester.ac.uk>
2017-04-26 10:14:19 +0100

    Move ramp() to utilityFunctions.f90, and remove facsimileFunctions.f90.

--- facsimileFunctions.f90
+++
@@ -1,7 +0,0 @@
-SUBROUTINE ramp (arg1, rampValue)
-  DOUBLE PRECISION arg1, arg2, rampValue
-
-  arg2 = ABS (arg1)
-  rampValue = (arg1 + arg2) / 2
-  RETURN
-END SUBROUTINE ramp
--- utilityFunctions.f90
+++ utilityFunctions.f90
@@ -43,3 +43,11 @@ SUBROUTINE zenith (theta, secx, cosx, ttime, dec)
 
   RETURN
 END SUBROUTINE zenith
+
+SUBROUTINE ramp (arg1, rampValue)
+  DOUBLE PRECISION arg1, arg2, rampValue
+
+  arg2 = ABS (arg1)
+  rampValue = (arg1 + arg2) / 2
+  RETURN
+END SUBROUTINE ramp

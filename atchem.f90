2a72d154dac51d173585ba1fe26f1e9704b2fb63
Cox, Samuel <sc676@leicester.ac.uk>
2017-05-03 16:04:12 +0100

    Remove testSpeciesList argument from matchNameToNumber(), use assumed-shape.

--- atchem.f90
+++ atchem.f90
@@ -226,7 +226,7 @@ PROGRAM ATCHEM
      prodIntName(i) = tempForProdIntName(i)
   ENDDO
 
-  CALL matchNameToNumber (speciesName, prodIntName, prodIntNameSize, returnArray, rateOfProdNS)
+  CALL matchNameToNumber (speciesName, prodIntName, returnArray, rateOfProdNS)
   ! prodArrayLen will hold the length of each line of prodIntSpecies
   ALLOCATE (prodArrayLen(rateOfProdNS))
   ALLOCATE (prodIntSpecies(rateOfProdNS, csize2))
@@ -244,7 +244,7 @@ PROGRAM ATCHEM
      reacIntName(i) = tempForReacIntName(i)
   ENDDO
 
-  CALL matchNameToNumber (speciesName, reacIntName, reacIntNameSize, returnArray, rateOfLossNS)
+  CALL matchNameToNumber (speciesName, reacIntName, returnArray, rateOfLossNS)
   ! lossArrayLen will hold the length of each line of reacIntSpecies
   ALLOCATE (lossArrayLen(rateOfLossNS))
   ALLOCATE (reacIntSpecies(rateOfLossNS, csize1))
@@ -429,8 +429,7 @@ PROGRAM ATCHEM
      speciesOutputRequired(i) = tempSpeciesOutputRequired(i)
   ENDDO
   ! fill SORNumber with the global numbering of the species found in speciesOutputRequired
-  CALL matchNameToNumber (speciesName, &
-                          speciesOutputRequired, speciesOutputRequiredSize, &
+  CALL matchNameToNumber (speciesName, speciesOutputRequired, &
                           tempSORNumber, SORNumberSize)
   ! Allocate SORNumber and fill from temporary array
   ALLOCATE (SORNumber(SORNumberSize))

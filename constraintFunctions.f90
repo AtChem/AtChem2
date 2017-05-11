8265c81d0f281be34c691b1467e7e37821d9789c
rs028 <rob.sommariva@gmail.com>
2017-05-11 10:28:43 +0100

    Rename calcPRESSfromM and Move some functions from constraintFunctions.f90

--- constraintFunctions.f90
+++ constraintFunctions.f90
@@ -53,36 +53,6 @@ CONTAINS
 
 ! ----------------------------------------------------------------- !
 
-  FUNCTION calcM (press, temp) result ( m )
-    ! calculate the number density of air (molecule cm-3) from
-    ! pressure (mbar) and temperature (K)
-    IMPLICIT NONE
-    real(kind=DP) :: press, temp, m
-    real(kind=DP) :: press_pa
-
-    press_pa = press*1.0d+2
-    m = (6.02214129d+23/8.3144621)*(press_pa/temp)
-    RETURN
-  END FUNCTION calcM
-
-! ----------------------------------------------------------------- !
-
-  SUBROUTINE calcDec(dec, t)
-    ! calculate the declination of the Sun as seen from Earth.
-    USE date, ONLY : secondsInYear, dayAsFractionOfYear
-    IMPLICIT NONE
-    real(kind=DP) :: dec, t, pi, daysInYear, maxDecInRad, currentFYear
-
-    daysInYear = 365.24
-    pi = 4.0*ATAN (1.0)
-    maxDecInRad = 23.44*pi/180.0
-    currentFYear = dayAsFractionOfYear + (t / secondsInYear)
-    dec=ASIN(SIN(-maxDecInRad)*COS((2.0*pi)*(currentFYear+(10.0/daysInYear))+2.0*0.0167*SIN(2*pi*(currentFYear-(2.0/daysInYear)))))
-    RETURN
-  END SUBROUTINE calcDec
-
-! ----------------------------------------------------------------- !
-
   SUBROUTINE addConstrainedSpeciesToProbSpec(z, x, numberOfConstrainedSpecies, constrainedSpecies, neq, constrainedConcs)
     real(kind=DP) :: z(*), x(*), constrainedConcs(*)
     INTEGER (kind=NPI) :: constrainedSpecies(*), zCounter, neq, numberOfConstrainedSpecies, i, j, speciesConstrained
--- conversionFunctions.f90
+++ conversionFunctions.f90
@@ -4,6 +4,50 @@ CONTAINS
 
 ! ----------------------------------------------------------------- !
 
+  SUBROUTINE calcDec(dec, t)
+    ! calculate the declination of the Sun as seen from Earth.
+    USE date, ONLY : secondsInYear, dayAsFractionOfYear
+    IMPLICIT NONE
+    real(kind=DP) :: dec, t, pi, daysInYear, maxDecInRad, currentFYear
+
+    daysInYear = 365.24
+    pi = 4.0*ATAN (1.0)
+    maxDecInRad = 23.44*pi/180.0
+    currentFYear = dayAsFractionOfYear + (t / secondsInYear)
+    dec=ASIN(SIN(-maxDecInRad)*COS((2.0*pi)*(currentFYear+(10.0/daysInYear))+2.0*0.0167*SIN(2*pi*(currentFYear-(2.0/daysInYear)))))
+    RETURN
+  END SUBROUTINE calcDec
+
+! ----------------------------------------------------------------- !
+
+  FUNCTION calcM (press, temp) result ( m )
+    ! calculate the number density of air (molecule cm-3) from
+    ! pressure (mbar) and temperature (K)
+    IMPLICIT NONE
+    real(kind=DP) :: press, temp, m
+    real(kind=DP) :: press_pa
+
+    press_pa = press*1.0d+2
+    m = (6.02214129d+23/8.3144621)*(press_pa/temp)
+    RETURN
+  END FUNCTION calcM
+
+! ----------------------------------------------------------------- !
+
+  FUNCTION calcPressure (m, temp) result ( press )
+    ! calculate pressure (mbar) from the number density of air
+    ! (molecule cm-3) and temperature (K)
+    IMPLICIT NONE
+    real(kind=DP) :: m, temp, press
+    real(kind=DP) :: press_pa
+
+    press_pa = (m*temp)/(6.02214129d+23/8.3144621)
+    press = press_pa/1.0d+2
+    RETURN
+  END FUNCTION calcPressure
+
+! ----------------------------------------------------------------- !
+
   FUNCTION convertRHtoH2O (rh, temp, press) result ( h2o )
     ! convert relative humidity to water concentration (molecule cm-3)
     ! pressure in mbar, temperature in K
@@ -18,20 +62,6 @@ CONTAINS
     h2o = 6.1078d0*exponent*e1
     RETURN
   END FUNCTION convertRHtoH2O
-
-! ----------------------------------------------------------------- !
-
-  FUNCTION calcPRESSfromM (m, temp) result ( press )
-    ! calculate pressure (mbar) from the number density of air
-    ! (molecule cm-3) and temperature (K)
-    IMPLICIT NONE
-    real(kind=DP) :: m, temp, press
-    real(kind=DP) :: press_pa
-
-    press_pa = (m*temp)/(6.02214129d+23/8.3144621)
-    press = press_pa/1.0d+2
-    RETURN
-  END FUNCTION calcPRESSfromM
 
 ! ----------------------------------------------------------------- !
 

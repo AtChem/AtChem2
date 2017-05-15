4fc22eabdfc552cf594e9e79af9c00699ac335b4
Cox, Samuel <sc676@leicester.ac.uk>
2017-05-15 08:58:46 +0100

    Finish formatting of utilityFunctions.f90.

--- utilityFunctions.f90
+++ utilityFunctions.f90
@@ -3,58 +3,67 @@ contains
   ! TODO: this is unused?
   subroutine temperature( temp, h2o, ttime )
     use types_mod
-    ! SUBROUTINE TO CALCULATE DIURNAL VARIATIONS IN TEMPERATURE
-    real(kind=DP) :: temp, ttime, rh, h2o, sin
+    implicit none
+    ! subroutine to calculate diurnal variations in temperature
+    real(kind=DP) :: temp, ttime, rh, h2o, sin, h2o_factor
 
-    temp = 289.86 + 8.3*SIN ((7.2722D-5*ttime)-1.9635)
+    temp = 289.86 + 8.3 * sin( ( 7.2722D-5 * ttime ) - 1.9635 )
     temp = 298.00
-    rh = 23.0*SIN ((7.2722D-5*ttime)+1.1781)+66.5
-    h2o = 6.1078*Dexp(-1.0D+0*(597.3-0.57*(temp-273.16))*18.0/1.986*(1.0/temp-1.0/273.16))*10./(1.38D-16*temp)*rh
+    rh = 23.0 * sin( ( 7.2722D-5 * ttime ) + 1.1781 ) + 66.5
+    h2o_factor = 10. / ( 1.38D-16 * temp ) * rh
+    h2o = 6.1078 * exp( -1.0D+0 * ( 597.3 - 0.57 * ( temp - 273.16 ) ) * 18.0 / 1.986 * ( 1.0 / temp - 1.0 / 273.16 ) ) * h2o_factor
 
     return
   end subroutine temperature
 
   subroutine atmosphere( o2, n2, m )
     use types_mod
+    implicit none
+    
     real(kind=DP) :: o2, n2, m
-    o2 = 0.2095*m
-    n2 = 0.7809*m
+
+    o2 = 0.2095 * m
+    n2 = 0.7809 * m
   end subroutine atmosphere
 
   subroutine zenith( theta, secx, cosx, ttime, dec )
     use zenithData
     use SZACalcVars
+    implicit none
+
     real(kind=DP) :: pi, radian, dec, ttime, theta, cosx, secx
     real(kind=DP) :: rampValue
     ! real(kind=DP) :: , sinld, cosld, lat, lha, longt
-    ! SOLAR decLINATION ANGLE FROM JULY 1ST - HARWELL TRAJ MODEL
+    ! solar declinatin angle from July 1st - Harwell traj model
 
     lat = latitude
     longt = longitude
-    pi = 4.0*ATAN (1.0)
-    ! LOCAL HOUR ANGLE - REPRESENTING TIME OF DAY
-    lha = (1 + ((ttime) / 4.32D+04)) * pi
-    radian = 180.0/pi
-    lat = lat/radian
-    ! dec = dec/RADIAN
-    theta = ACOS (COS (LHA)*COS (dec)*COS (lat)+SIN (dec)*SIN (lat)+1.0D-30)
-    sinld = SIN (lat)*SIN (dec)
-    cosld = COS (lat)*COS (dec)
+    pi = 4.0 * atan( 1.0 )
+    ! local hour angle - representing time of day
+    lha = ( 1.0 + ( ttime / 4.32D+04 ) ) * pi
+    radian = 180.0 / pi
+    lat = lat / radian
+    ! dec = dec/radian
+    theta = acos( cos( lha ) * cos( dec ) * cos( lat ) + sin( dec ) * sin( lat ) + 1.0D-30 )
+    sinld = sin( lat ) * sin( dec )
+    cosld = cos( lat ) * cos( dec )
 
-    call ramp( COS (lha)*cosld+sinld, rampValue )
-    secx = 1/ (rampValue + 1.00d-30)
+    call ramp( cos( lha ) * cosld + sinld, rampValue )
+    secx = 1.0 / ( rampValue + 1.00d-30 )
 
-    cosx = (COS (lha)*cosld) + sinld
+    cosx = cos( lha ) * cosld + sinld
 
     return
   end subroutine zenith
 
   subroutine ramp( arg1, rampValue )
     use types_mod
+    implicit none
+
     real(kind=DP) :: arg1, arg2, rampValue
 
-    arg2 = ABS (arg1)
-    rampValue = (arg1 + arg2) / 2
+    arg2 = abs( arg1 )
+    rampValue = ( arg1 + arg2 ) / 2
     return
   end subroutine ramp
 end module utilityFunctions_mod

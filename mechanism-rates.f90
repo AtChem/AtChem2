efd7b6aa6c9490e151c2e89321618758b6db9704
Cox, Samuel <sc676@leicester.ac.uk>
2017-05-17 14:03:18 +0100

    Change getEnvVarsAtT() to only output via currentEnvVarValues, so this needs to be used after getEnvVarsAtT() is called.

--- mechanism-rates.f90
+++ mechanism-rates.f90
@@ -2,15 +2,17 @@ module mechanismRates_mod
 
 contains
   subroutine mechanism_rates( p, t, y, mnsp )
+    use, intrinsic :: iso_fortran_env, only : stderr => error_unit
     use types_mod
+    use storage, only : maxEnvVarNameLength
     use photolysisRates
     use zenithData1, only : cosX, secX
     use constraints
-    use envVars, only : ro2
+    use envVars, only : ro2, envVarNames, currentEnvVarValues
     use interpolationFunctions_mod, only : getConstrainedQuantAtT
     use interpolationMethod, only : getConditionsInterpMethod
     use outputFunctions_mod, only : ro2sum
-    use constraintFunctions_mod, only : getEnvVarsAtT
+    use constraintFunctions_mod, only : getEnvVarsAtT, getEnvVarNum
     use utilityFunctions_mod, only : atmosphere
     implicit none
 
@@ -19,10 +21,11 @@ contains
     real(kind=DP), intent(in) :: t
     integer(kind=NPI), intent(in) :: mnsp
     real(kind=DP), intent(in) :: y(mnsp)
-    real(kind=DP) :: temp, pressure, dummy
+    real(kind=DP) :: temp, pressure, dummy, this_env_val
 
     integer(kind=NPI) :: i
     real(kind=DP) :: photoRateAtT
+    character(len=maxEnvVarNameLength) :: this_env_var_name
 
     include 'modelConfiguration/mechanism-rate-declarations.f90'
 
@@ -31,7 +34,37 @@ contains
 
     dec = -1e16
 
-    call getEnvVarsAtT( t, temp, rh, h2o, dec, pressure, m, blh, dilute, jfac, roofOpen )
+    call getEnvVarsAtT( t )
+
+    do i = 1, size( envVarNames )
+      this_env_var_name = envVarNames(i)
+      this_env_val = currentEnvVarValues(getEnvVarNum(this_env_var_name))
+      select case ( this_env_var_name )
+        case ( 'TEMP' )
+          temp = this_env_val
+        case ( 'RH' )
+          rh = this_env_val
+        case ( 'H2O' )
+          h2o = this_env_val
+        case ( 'DEC' )
+          dec = this_env_val
+        case ( 'PRESS' )
+          pressure = this_env_val
+        case ( 'M' )
+          m = this_env_val
+        case ( 'BLHEIGHT' )
+          blh = this_env_val
+        case ( 'DILUTE' )
+          dilute = this_env_val
+        case ( 'JFAC' )
+          jfac = this_env_val
+        case ( 'ROOFOPEN' )
+          roofOpen = this_env_val
+        case default
+          write(stderr,*) 'getEnvVarsAtT(): invalid environment name ' // trim( this_env_var_name )
+          stop
+      end select
+    end do
 
     call atmosphere( o2, n2, m )
 

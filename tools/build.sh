2628e9199e45bf6e1dea204ba2cba5e18f476728
Cox, Samuel <sc676@leicester.ac.uk>
2017-05-18 10:02:49 +0100

    Add a second argument to tools/build.sh, tools/setup_atchem.py, and tools/mech_converter.py:convert(), denoting where the mechanism-rate-coefficients/declarations.f90 files should go when built (which now defaults to '.' , and there's no real way to pass this on to the Makefile, so the Makefile will break if you send this anywhere else).

--- tools/build.sh
+++ tools/build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 # $1 is the input file
+# $2 is the directory in which to place the mechanism-rate-coefficients.f90 and
+#    mechanism-rate-declarations.f90 files. This should point to the base directory
+#    of the source files of AtChem.
 set -e
 echo ''
 echo $1
+echo $2
 echo "call ./tools/setup_atchem.py"
-python ./tools/setup_atchem.py $1
+python ./tools/setup_atchem.py $1 $2
 echo ''
 echo "make" $1
 make --always-make

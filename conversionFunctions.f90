31e70a1d31f3e3171c28932c7dfb0061529989ac
rs028 <rob.sommariva@gmail.com>
2017-05-30 18:16:04 +0100

    Document RH conversion function

--- conversionFunctions.f90
+++ conversionFunctions.f90
@@ -41,14 +41,24 @@ contains
   pure function convertRHtoH2O( rh, temp, press ) result ( h2o )
     ! convert relative humidity to water concentration (molecule cm-3)
     ! pressure in mbar (1 mbar = 1 hPa), temperature in K
+    !
+    ! from "Humidity Conversion Formulas" published by Vaisala (2013)
     implicit none
 
     real(kind=DP), intent(in) :: rh, temp, press
     real(kind=DP) :: h2o, h2o_ppm, temp_c, wvp
 
+    ! convert temperature to celsius; use eq.6 to calculate the water
+    ! vapour saturation pressure; use eq.1 to calculate the water
+    ! vapour pressure from relative humidity (see Vaisala paper)
     temp_c = temp - 273.15
-    wvp = (rh/100) * 6.116441 * 10**( (7.591386 * temp_c) / (temp_c + 240.7263) )
+    wvp = ( rh/100 ) * ( 6.116441 * 10**((7.591386 * temp_c)/(temp_c + 240.7263)) )
+
+    ! calculate volume of water vapour per volume of dry air using
+    ! eq.18 (see Vaisala paper)
     h2o_ppm = 1.0d+06 * wvp / (press - wvp)
+
+    ! convert ppm to molecule cm-3
     h2o = h2o_ppm * calcM(press, temp) * 1.0d-06
 
     return

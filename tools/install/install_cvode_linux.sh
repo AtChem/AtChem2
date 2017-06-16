373b6a82720d4157bbcb9cbf61a98a0037476dc8
rs028 <rob.sommariva@gmail.com>
2017-06-16 15:56:42 +0100

    Clean up and add argument check to install scripts

--- tools/install/install_cvode_linux.sh
+++ tools/install/install_cvode_linux.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# This script downloads and installs CVODE 2.9.0 into the directory given by input
-# argument $1. This is dependent on the existence of a gcc installation.
+# This script downloads and installs CVODE 2.9.0 into the directory
+# given by input argument $1. This is dependent on the existence of a
+# gcc installation.
 #
-# This is the LINUX/UNIX version.
+# This is the Linux/Unix version.
 #
 # Edit FORT_COMP to point to an existing gfortran or ifort executable.
 #
 # Example usage:
 #   ./install_cvode_linux.sh /path/to/install/directory
+
 FORT_COMP=/usr/bin/gfortran
 
 if [ -z "$1" ] ; then
@@ -19,8 +21,11 @@ cd $1
 wget https://computation.llnl.gov/projects/sundials/download/cvode-2.9.0.tar.gz
 
 tar -zxf cvode-2.9.0.tar.gz
+rm cvode-2.9.0.tar.gz
+
 cd cvode-2.9.0/
 mkdir build
+
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$1/cvode \
     -DCMAKE_C_COMPILER:FILEPATH=gcc \
--- tools/install/install_numdiff.sh
+++ tools/install/install_numdiff.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-# This script downloads and installs NUMDIFF 5.8.1 into the directory given by input
-# argument $1. This is dependent on the existence of a gcc installation.
+# This script downloads and installs NUMDIFF 5.8.1 into the directory
+# given by input argument $1. This is dependent on the existence of a
+# gcc installation.
 #
 # Example usage:
-#   ./install_numdiff_linux.sh /path/to/install/directory
+#   ./install_numdiff.sh /path/to/install/directory
 
+if [ -z "$1" ] ; then
+  echo "Please provide an argument to tools/install/install_numdiff.sh"
+  exit 1
+fi
 cd $1
 wget http://ftp.igh.cnrs.fr/pub/nongnu/numdiff/numdiff-5.8.1.tar.gz
 
 tar -zxf numdiff-5.8.1.tar.gz
+rm numdiff-5.8.1.tar.gz
+
 cd numdiff-5.8.1/
 ./configure --prefix=$1/numdiff
 make

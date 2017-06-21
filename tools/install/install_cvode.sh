ab4258354a7d44dc23feebc004684c9a820b9f9b
Cox, Samuel <sc676@leicester.ac.uk>
2017-06-21 15:28:47 +0100

    Rename to install_cvode.sh, and remove install_cvode_linux.sh. Add comments.

--- tools/install/install_cvode.sh
+++ tools/install/install_cvode.sh
@@ -3,20 +3,18 @@
 # This script downloads and installs CVODE 2.9.0 into the directory
 # given by input argument $1. Using the fortran compiler executable
 # given by input argument $2.
-#
-# This is the macOSX version.
-#
+##
 # Example usage:
-#   ./install_cvode_osx.sh /path/to/install/directory /path/to/fortran/compiler
+#   ./install_cvode.sh /path/to/install/directory /path/to/fortran/compiler
 
+# Query the operating system, and set LAPACK_LIBS to the default location
 OS=$(uname -s)
-echo $OS
 if [ "$OS" == 'Darwin' ]; then
   LAPACK_LIBS=/usr/lib/liblapack.dylib:/usr/lib/libblas.dylib
 else
   LAPACK_LIBS=/usr/lib/liblapack.so:/usr/lib/libblas.so
 fi
-echo $LAPACK_LIBS
+# Ensure a first argument is provided, and that it is an existing directory
 if [ -z "$1" ] ; then
   echo "Please provide an argument to tools/install/install_cvode_osx.sh"
   echo "Example usage: ./install_cvode_osx.sh /path/to/install/directory /path/to/fortran/compiler"
@@ -26,6 +24,9 @@ elif [ ! -d "$1" ]; then
   echo "Example usage: ./install_cvode_osx.sh /path/to/install/directory /path/to/fortran/compiler"
   exit 1
 fi
+# If a second argument is provided, check that it is an existing file.
+# If it's not provided, then give it a default, so long as that also
+# exists.
 if [ -z "$2" ] ; then
   if [ -f /usr/bin/gfortran ] ; then
     echo "Default fortran compiler executable selected. To choose another compiler, provide this as a second argument."
@@ -44,17 +45,17 @@ else
     exit 1
   fi
 fi
+# Move to provided directory
 cd $1
-ls -al /usr/bin
-wget https://computation.llnl.gov/projects/sundials/download/cvode-2.9.0.tar.gz
 
+wget https://computation.llnl.gov/projects/sundials/download/cvode-2.9.0.tar.gz
 tar -zxf cvode-2.9.0.tar.gz
 rm cvode-2.9.0.tar.gz
-
 cd cvode-2.9.0/
-mkdir build
 
+mkdir build
 cd build
+
 cmake -DCMAKE_INSTALL_PREFIX=$1/cvode \
     -DCMAKE_C_COMPILER:FILEPATH=gcc \
     -DCMAKE_Fortran_COMPILER=$FORT_COMP \

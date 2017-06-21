6e473cdca05b74f999c1d8d8b7c92fcf16f06d3b
Cox, Samuel <sc676@leicester.ac.uk>
2017-06-21 12:34:54 +0100

    Add libblas and liblapack to travis macOS cvode installer.

--- travis/install_cvode_osx.sh
+++ travis/install_cvode_osx.sh
@@ -2,7 +2,7 @@
 
 # $1 is the directory to save cvode into
 if [ -z "$1" ] ; then
-  echo "Please provide an argument to travis/install_cvode_linux.sh"
+  echo "Please provide an argument to travis/install_cvode_osx.sh"
   exit 1
 fi
 cd $1
@@ -16,6 +16,7 @@ cmake -DCMAKE_INSTALL_PREFIX=/Users/travis/build/AtChem/AtChem/cvode \
     -DCMAKE_C_COMPILER:FILEPATH=gcc \
     -DCMAKE_Fortran_COMPILER=/usr/local/Cellar/gcc@4.8/4.8.5/bin/gfortran-4.8 \
     -DLAPACK_ENABLE:BOOL=ON \
+    -DLAPACK_LIBRARIES=/usr/lib/liblapack.so:/usr/lib/libblas.so \
     -DFCMIX_ENABLE:BOOL=ON \
     -DEXAMPLES_ENABLE:BOOL=OFF \
     -DCMAKE_MACOSX_RPATH:BOOL=ON \

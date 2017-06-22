fb2a45fa0419aee6d8d6cf87d945079ec5452eb8
rs028 <rob.sommariva@gmail.com>
2017-06-22 15:51:32 +0100

    Make build.sh more flexible

--- tools/build.sh
+++ tools/build.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
-
+#
 # $1 is the input file
 # $2 is the directory in which to place the mechanism-rate-coefficients.f90 and
-#    mechanism-rate-declarations.f90 files. This should point to the base directory
-#    of the source files of AtChem.
+#    mechanism-rate-declarations.f90 files. This, by default, points
+#    to the base directory of the source files of AtChem.
+# $3 is ...
+
 set -e
 echo ''
 echo $1
 echo $2
 echo $3
-echo "call ./tools/setup_atchem.py"
-python ./tools/setup_atchem.py $1 $2 $3
+
+if [ "${PWD##*/}" == "tools" ]; then
+    echo "call ./setup_atchem.py"
+    python ./setup_atchem.py $1 $2 $3
+else
+    echo "call ./tools/setup_atchem.py"
+    python ./tools/setup_atchem.py $1 $2 $3
+fi
+
 echo ''
 echo "make" $1
 make --always-make

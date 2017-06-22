6a52d1e42addb5bf93cdb9600c8f41a81e1b619c
rs028 <rob.sommariva@gmail.com>
2017-06-22 16:35:47 +0100

    Remove if construct

--- tools/build.sh
+++ tools/build.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 #
-# $1 is the input file
-# $2 is the directory in which to place the mechanism-rate-coefficients.f90 and
-#    mechanism-rate-declarations.f90 files. This, by default, points
-#    to the base directory of the source files of AtChem.
-# $3 is ...
+# Script to ....
+#
+# $1 is the mechanism input file (*.fac)
+#
+# $2 is the directory in which to place the output files:
+#    - mechanism-rate-coefficients.f90
+#    - mechanism-rate-declarations.f90
+#    By default, it is the base directory of AtChem.
+#
+# $3 is the directory in which to place the output files:
+#    - mechanism.prod
+#    - mechanism.reac
+#    - mechanism.species
+#    By default it is modelConfiguration/ in the base directory of AtChem.
 
 set -e
 echo ''
-echo $1
-echo $2
-echo $3
+echo "input file:" $1
+echo "output directory:" $2
+echo "output directory:" $3
 
-if [ "${PWD##*/}" == "tools" ]; then
-    echo "call ./setup_atchem.py"
-    python ./setup_atchem.py $1 $2 $3
-else
-    echo "call ./tools/setup_atchem.py"
-    python ./tools/setup_atchem.py $1 $2 $3
-fi
+echo ''
+echo "call setup_atchem.py"
+python ${BASH_SOURCE%/*}/setup_atchem.py $1 $2 $3
 
 echo ''
 echo "make" $1

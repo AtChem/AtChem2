f8916442b339fb41341e290b75babcae7b69263c
R. Sommariva <rob.sommariva@gmail.com>
2017-06-22 20:33:43 +0100

    Make build.sh more flexible (#240)
    Make build.sh more verbose and better documented. Add tools/multiplot.py.

--- tools/build.sh
+++ tools/build.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
+#
+# Script to process the mechanism file (*.fac) and convert it to the
+# correct format for AtChem.
+#
+# N.B.: the script MUST be run from the base directory of AtChem.
+#
+# $1 is the mechanism input file
+#
+# $2 is the directory in which to place the output files:
+#    - mechanism-rate-coefficients.f90
+#    - mechanism-rate-declarations.f90
+#    By default, it is the base directory of AtChem.
+#
+# $3 is the directory in which to place the output files:
+#    - mechanism.prod
+#    - mechanism.reac
+#    - mechanism.species
+#    By default it is modelConfiguration/ in the base directory of AtChem.
 
-# $1 is the input file
-# $2 is the directory in which to place the mechanism-rate-coefficients.f90 and
-#    mechanism-rate-declarations.f90 files. This should point to the base directory
-#    of the source files of AtChem.
 set -e
 echo ''
-echo $1
-echo $2
-echo $3
-echo "call ./tools/setup_atchem.py"
+echo "input mechanism file:" $1
+echo "fortran output directory:" $2
+echo "mechanism output directory:" $3
+
+echo ''
+echo "call setup_atchem.py"
 python ./tools/setup_atchem.py $1 $2 $3
+
 echo ''
 echo "make" $1
 make --always-make

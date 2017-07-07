9511daaa449792e600c9d8cc7419d04d960931b9
Cox, Samuel <sc676@leicester.ac.uk>
2017-07-07 14:11:56 +0100

    Re-add license. May need to move bash delcaration to top.

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -1,3 +1,14 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2017 Sam Cox, Roberto Sommariva
+#
+# This file is part of the AtChem software package.
+#
+# This file is covered by the MIT license which can be found in the file
+# LICENSE.md at the top level of the AtChem distribution.
+#
+# -----------------------------------------------------------------------------
+
 #!/bin/bash
 #set -e
 

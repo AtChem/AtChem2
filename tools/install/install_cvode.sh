50833c217487cca74336a064cda9ad9fed049843
Sam Cox <sc676@le.ac.uk>
2018-06-14 10:23:23 +0100

    Correct lapack/blas locations in cvode install.

--- tools/install/install_cvode.sh
+++ tools/install/install_cvode.sh
@@ -23,7 +23,7 @@ OS=$(uname -s)
 if [ "$OS" == 'Darwin' ]; then
   LAPACK_LIBS=/usr/lib/liblapack.dylib:/usr/lib/libblas.dylib
 else
-  LAPACK_LIBS=/usr/lib/liblapack.dylib:/usr/lib/libblas.dylib
+  LAPACK_LIBS=/usr/lib/liblapack.so:/usr/lib/libblas.so
 fi
 # Ensure a first argument is provided, and that it is an existing directory
 if [ -z "$1" ] ; then

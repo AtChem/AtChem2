c6e8b38d5f9ecd437bd80271ebee18eeb26c748c
Sam Cox <sc676@le.ac.uk>
2018-07-10 10:53:18 +0100

    More tidying.

--- travis/unit_tests/atmosphere_test.f90
+++ travis/unit_tests/atmosphere_test.f90
@@ -13,25 +13,25 @@ contains
     real(kind=DP) :: m, o2, n2
 
     m = 1.0_DP
-    call calcAtmosphere(m,o2,n2)
+    call calcAtmosphere( m, o2, n2 )
 
-    call assert_true( m == 1.0_DP, "m unchanged 1")
-    call assert_true( o2 == 0.2095_DP, "o2 correct 1")
-    call assert_true( n2 == 0.7809_DP, "n2 correct 1")
+    call assert_true( m == 1.0_DP, "m unchanged 1" )
+    call assert_true( o2 == 0.2095_DP, "o2 correct 1" )
+    call assert_true( n2 == 0.7809_DP, "n2 correct 1" )
 
     m = 2.0_DP
-    call calcAtmosphere(m,o2,n2)
+    call calcAtmosphere( m, o2, n2 )
 
-    call assert_true( m == 2.0_DP, "m unchanged 2")
-    call assert_true( o2 == 0.4190_DP, "o2 correct 2")
-    call assert_true( n2 == 1.5618_DP, "n2 correct 2")
+    call assert_true( m == 2.0_DP, "m unchanged 2" )
+    call assert_true( o2 == 0.4190_DP, "o2 correct 2" )
+    call assert_true( n2 == 1.5618_DP, "n2 correct 2" )
   end subroutine test_calcAtmosphere
 
   subroutine test_zero_equal
     use types_mod
     implicit none
 
-    call assert_true(0.0_DP == 0.0_DP, "Zero equality - sanity check!")
+    call assert_true( 0.0_DP == 0.0_DP, "Zero equality - sanity check!" )
   end subroutine test_zero_equal
 
 end module atmosphere_test

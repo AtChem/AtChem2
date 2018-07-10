29d2d594ea04bbea4db537e0781262752a9f8eb5
Sam Cox <sc676@le.ac.uk>
2018-07-10 10:48:40 +0100

    Tidy up atmosphere_test.f90.

--- travis/unit_tests/atmosphere_test.f90
+++ travis/unit_tests/atmosphere_test.f90
@@ -2,10 +2,8 @@ module atmosphere_test
   use fruit
   use types_mod
   implicit none
-  real(kind=DP) :: abs_tol = 0.1
 
 contains
-
 
   subroutine test_calcAtmosphere
     use types_mod
@@ -20,6 +18,7 @@ contains
     call assert_true( m == 1.0_DP, "m unchanged 1")
     call assert_true( o2 == 0.2095_DP, "o2 correct 1")
     call assert_true( n2 == 0.7809_DP, "n2 correct 1")
+
     m = 2.0_DP
     call calcAtmosphere(m,o2,n2)
 
@@ -34,4 +33,5 @@ contains
 
     call assert_true(0.0_DP == 0.0_DP, "Zero equality - sanity check!")
   end subroutine test_zero_equal
+
 end module atmosphere_test

d9d11b3b8c91c8605402125e78cb8dec76fdcc91
rs028 <rob.sommariva@gmail.com>
2018-07-16 11:51:02 +0100

    Changes to FRUIT installation

--- travis/unit_tests/fruit_generator.rb
+++ travis/unit_tests/fruit_generator.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'fruit_processor'
 if ENV['TRAVIS'] == nil
-  load "/Users/sc676/Downloads/fruit_3.4.3/rake_base.rb"
+  load "~/libs/fruit_3.4.3/rake_base.rb"
 else
   if ENV['TRAVIS_OS_NAME'] == 'linux'
     load "/home/travis/build/AtChem/AtChem2/fruit/fruit_3.4.3/rake_base.rb"

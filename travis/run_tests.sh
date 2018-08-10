0ed0e1245cc82e59bc1f3fcf6fc2ecdc161de104
Sam Cox <sc676@le.ac.uk>
2018-08-10 10:59:23 +0100

    Remove indent-script call from run_tests.sh. This is a fix to #329.

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -64,36 +64,6 @@ TESTS_DIR=travis/tests
 RESULTS_FILE=$TESTS_DIR/testsuite.log
 export DYLD_LIBRARY_PATH=$2
 
-echo "Running indent script on:"
-for file in src/*.f90 ; do
-  echo $file
-  python ./tools/fix_indent.py $file $file.cmp &>/dev/null
-  this_indent_file_failures=$(diff -q $file $file.cmp)
-  exitcode=$?
-  rm $file.cmp
-  if [ $exitcode -eq 1 ]; then
-    failed_indent="$failed_indent
-
-$this_indent_file_failures"
-    echo $file 'FAILED'
-  elif [ $exitcode -ne 0 ]; then
-    echo 'diff gave an error on' $file '. Aborting.'
-    exit 1
-  fi
-done
-
-echo ""
-if [ -z "$failed_indent" ]; then
-  echo "Indent test PASSED"
-  indent_test_passed=0
-else
-  echo "Indent test FAILED"
-  echo "$failed_indent" >> $RESULTS_FILE
-  indent_test_passed=1
-fi
-echo "Indent script finished"
-echo ""
-
 echo "travis/run_tests.sh: Tests to be run:" $1
 
 # initialise counters

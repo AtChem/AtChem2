db4573cb9a7f0531ddd75e37286613d5893588b0
Sam Cox <sc676@le.ac.uk>
2019-01-02 16:45:12 +0000

    Move FunctionParser.f90 out of deps/, place directly in src/.

--- dep/FortranParser-2.0.3/cmake/GNU_Fortran_Flags.cmake
+++
@@ -1,6 +0,0 @@
-MESSAGE ( STATUS "Adding default compiler flags for GNU compiler")
-#set ( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wall -g -O0 -cpp -pedantic -fcheck=all -Wextra -fbacktrace -fbounds-check -ffree-line-length-none" )
-set ( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -O3 -cpp -ffree-line-length-none" )
-set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O3" )
-
-set ( Fortran_FLAGS_ARE_SET ON )

d40883c852199bf532d39aa0dc6de57249779956
Sam Cox <sc676@le.ac.uk>
2019-01-31 13:21:19 +0000

    Add more to the  help, and tidy up formatting of source.

--- src/cli.f90
+++ src/cli.f90
@@ -10,10 +10,11 @@ module helper_routines
               [ flag('--help',              'Displays this help message.'), &
                 flag('--model',             'The base directory of the model.'), &
                 flag('--output',            'The destination directory for output.'), &
-                flag('--reactionRates', 'The destination directory for the reactionRates output.'), &
-                flag('--configuration', 'The directory of the model configuration.'), &
-                flag('--constraints', 'The base directory of constraints. ' // &
-                                      'This typically contains 3 subdirectories: environment, photolysis, and species.'), &
+                flag('--reactionRates',     'The destination directory for the reactionRates output. ' // &
+                                            'By default this is a subdirectory of the output directory.'), &
+                flag('--configuration',     'The directory of the model configuration.'), &
+                flag('--constraints',       'The base directory of constraints. ' // &
+                                            'This typically contains 3 subdirectories: environment, photolysis, and species.'), &
                 flag('--spec_constraints ', 'The directory containing species constraints data.'), &
                 flag('--env_constraints  ', 'The directory containing environment constraints data.'), &
                 flag('--photo_constraints', 'The directory containing photolysis constraints data.') ]
@@ -25,156 +26,160 @@ contains
 
     integer i
 
-    write(*,*) 'This is the help message.'
-    write(*,*) 'Possible input flags are: '
+    write(*,*) new_line('A')//' This is the help message.'
+
+    write(*,'(A)', advance='no') ' Usage: ./atchem '
+    write(*,'(3A)', advance='no') '[', trim(valid_flags(1)%flag_switch), '] '
+    do i=2,size(valid_flags)
+      write(*,'(5A)', advance='no') '[', trim(valid_flags(i)%flag_switch), '=', &
+                                    trim(valid_flags(i)%flag_switch(3:))//'_dir', '] '
+    end do
+
+    write(*,*) new_line('A')//' Possible input flags are: '
     do i=1,size(valid_flags)
       write(*,*) trim(valid_flags(i)%flag_switch)
-      write(*,*) '     ', valid_flags(i)%flag_help
-      write(*,*)
+      write(*,*) '     ', valid_flags(i)%flag_help//new_line('A')
     end do
-    write(*,*) 'End of help message.'
+
+    write(*,*) 'In essence, the directories default to sit in the following tree. '
+    write(*,*) 'Modification via the input parameters cascades to lower directories.'
+    write(*,*)
+    write(*,*) '                                 model_dir'
+    write(*,*) '                                      | '
+    write(*,*) '     +--------------------------------+--------------------------+'
+    write(*,*) '     |                                |                          |'
+    write(*,*) ' output_dir                     constraints_dir           configuration_dir'
+    write(*,*) '     |                                                           |'
+    write(*,*) 'reactionRates_dir                       +------------------------+----------------------+'
+    write(*,*) '                                        |                        |                      |'
+    write(*,*) '                              env_constraints_dir   photolysis_constraints_dir  species_constraints_dir'
+    write(*,*)
   end subroutine print_help
-! split a string into 2 either side of a delimiter token
-SUBROUTINE split_string(instring, string1, string2, delim)
-  implicit none
-  CHARACTER(100) :: instring
-  CHARACTER :: delim
-  CHARACTER(100),INTENT(OUT):: string1,string2
-  INTEGER :: index
 
-  instring = TRIM(instring)
+  ! split a string into 2 either side of a delimiter token
+  subroutine split_string(instring, string1, string2, delim)
+    implicit none
+    character(100) :: instring
+    character :: delim
+    character(100), intent(out) :: string1,string2
+    integer :: index
 
-  index = SCAN(instring,delim)
-  if ( index /= 0 ) then
-    if ( index == 1 ) then
-      string1 = ''
-      string2 = instring(2:)
-else if ( index == len(instring) ) then
-      string1 = instring(1:index-1)
+    instring = trim(instring)
+
+    index = scan(instring,delim)
+    if ( index /= 0 ) then
+      if ( index == 1 ) then
+        string1 = ''
+        string2 = instring(2:)
+      else if ( index == len(instring) ) then
+        string1 = instring(1:index-1)
+        string2 = ''
+      else
+        string1 = instring(1:index-1)
+        string2 = instring(index+1:)
+      end if
+    else
+      if ( instring /= '--help' ) then
+        write(*,*) 'delimiter not found'
+      end if
+      string1 = instring
       string2 = ''
-    else
-      string1 = instring(1:index-1)
-      string2 = instring(index+1:)
-  end if
-  else
-    if ( instring /= '--help' ) then
-      write(*,*) 'delimiter not found'
+    endif
+
+  end subroutine split_string
+
+
+  subroutine check_name_value_pair_validity(name, value, name_valid, value_valid)
+    implicit none
+
+    character(100), intent(in) :: name, value
+    logical, intent(out) :: name_valid, value_valid
+
+    name_valid = .true.
+    ! Check validity of name
+    if ( index(name, '--', back=.true.) /= 1 ) then
+      if  ( index(name, '--', back=.true.) == 0 ) then
+        write(*,*) '-- does not occur in "', trim(name), '"'
+        name_valid = .false.
+      else
+        write(*,*) 'name "', trim(name), '" is not valid - too many or incorrectly placed --s'
+        name_valid = .false.
+      end if
     end if
-    string1 = instring
-    string2 = ''
-  endif
-  ! write(*,*) string1
-  ! write(*,*) string2
-END SUBROUTINE split_string
 
-SUBROUTINE check_name_value_pair_validity(name, value, name_valid, value_valid)
-  implicit none
-
-  CHARACTER(100),INTENT(IN):: name, value
-  LOGICAL, intent(out) :: name_valid, value_valid
-
-  name_valid = .true.
-  ! Check validity of name
-  if ( index(name, '--', back=.true.) == 1 ) then
-    write(*,*) 'name "', trim(name), '" is valid ', index(name, '--', back=.true.)
-  else if  ( index(name, '--', back=.true.) == 0 ) then
-    write(*,*) '-- does not occur in "', trim(name), '"'
-    name_valid = .false.
-  else
-    write(*,*) 'name "', trim(name), '" is not valid - too many or incorrectly placed --s'
-    name_valid = .false.
-  end if
-
-  value_valid = .true.
-  ! Check validity of value - just check it hasn't got any spaces
-  ! TODO: note that this can't really fail unless empty
-  if ( len(trim(value)) == 0) then
-    if ( trim(name) /= '--help' ) then
-      write(*,*) 'value is empty'
+    value_valid = .true.
+    ! Check validity of value - just check it hasn't got any spaces
+    ! TODO: note that this can't really fail unless empty
+    if ( len(trim(value)) == 0) then
+      if ( trim(name) /= '--help' ) then
+        write(*,*) 'flag "', trim(name), '" has no associated value supplied.'
+        value_valid = .false.
+      end if
+    end if
+    if ( index(trim(value), ' ') /= 0 ) then
+      write(*,*) 'value "', value, '" is invalid as it contains a space'
       value_valid = .false.
-    else
-      write(*,*) 'value is empty, but that is ok as name == --help'
     end if
-  end if
-  if ( index(trim(value), ' ') /= 0 ) then
-    write(*,*) 'value "', value, '" is invalid as it contains a space'
-    value_valid = .false.
-  else
-    write(*,*) 'value "', trim(value), '" is valid'
-  end if
-  if ( name_valid .and. value_valid ) then
-    write(*,*) 'pair is valid'
-  else
-    write(*,*) 'pair is not valid'
-  end if
-END SUBROUTINE check_name_value_pair_validity
 
-function array_contains(array, value) result (index)
-  implicit none
-  character(100), intent(in) :: array(:), value
-  integer :: index
-  integer :: i
+  end subroutine check_name_value_pair_validity
 
-  ! write(*,*) size(array)
-  ! write(*,*) value
-  if ( size(array) > 0 ) then
-    do i=1,size(array)
-      ! write(*,*) 'loop'
-      ! write(*,*) i, ' ', trim(array(i)), ' ', trim(value)
-      ! write(*,*) i, ' ', array(i), ' ', value
-      if ( trim(array(i)) == trim(value) ) then
-        index = i
-        ! write(*,*) 'found', i, index
-        return
-      end if
-    end do
-  end if
-  index = 0
-  ! write(*,*) 'not found', index
-  return
-end function array_contains
 
-function flag_array_contains(array, value) result (index)
-  implicit none
-  type(flag), intent(in) :: array(:)
-  character(100), intent(in) :: value
-  integer :: index
-  integer :: i
+  function array_contains(array, value) result (index)
+    implicit none
+    character(100), intent(in) :: array(:), value
+    integer :: index
+    integer :: i
 
-  ! write(*,*) size(array)
-  ! write(*,*) value
-  if ( size(array) > 0 ) then
-    do i=1,size(array)
-      ! write(*,*) 'loop'
-      ! write(*,*) i, ' ', trim(array(i)), ' ', trim(value)
-      ! write(*,*) i, ' ', array(i), ' ', value
-      if ( trim(array(i)%flag_switch) == trim(value) ) then
-        index = i
-        ! write(*,*) 'found', i, index
-        return
-      end if
-    end do
-  end if
-  index = 0
-  ! write(*,*) 'not found', index
-  return
-end function flag_array_contains
+    if ( size(array) > 0 ) then
+      do i=1,size(array)
+        if ( trim(array(i)) == trim(value) ) then
+          index = i
+          return
+        end if
+      end do
+    end if
+    index = 0
 
-function read_value_or_default( output_name, default, names, values ) result ( out )
-  implicit none
-  character(len=*), intent(in) :: output_name, default, names(:), values(:)
-  character(len=100) :: out
-  integer :: loc
+    return
+  end function array_contains
 
-  loc = array_contains(names, output_name)
 
-  if ( loc /= 0 ) then
-    out = trim(values(loc))
-  else
-    out = trim(default)
-  end if
+  function flag_array_contains(array, value) result (index)
+    implicit none
+    type(flag), intent(in) :: array(:)
+    character(100), intent(in) :: value
+    integer :: index
+    integer :: i
 
-end function  read_value_or_default
+    if ( size(array) > 0 ) then
+      do i=1,size(array)
+        if ( trim(array(i)%flag_switch) == trim(value) ) then
+          index = i
+          return
+        end if
+      end do
+    end if
+    index = 0
+
+    return
+  end function flag_array_contains
+
+
+  function read_value_or_default( output_name, default, names, values ) result ( out )
+    implicit none
+    character(len=*), intent(in) :: output_name, default, names(:), values(:)
+    character(len=100) :: out
+    integer :: loc
+
+    loc = array_contains(names, output_name)
+
+    if ( loc /= 0 ) then
+      out = trim(values(loc))
+    else
+      out = trim(default)
+    end if
+
+  end function  read_value_or_default
 
 end module helper_routines
 
@@ -188,15 +193,14 @@ end module helper_routines
 
 
 
-PROGRAM CLI
+program cli
 
   use, intrinsic :: iso_fortran_env, only : stderr => error_unit
   use helper_routines
   implicit none
 
-  integer :: cmd_arg_count, i, loc
+  integer :: cmd_arg_count, i
   character(len=100), allocatable :: input_strings(:), names(:), values(:)
-  ! character(len=100) :: valid_flags(9)
   character(len=100) :: model_dir, output_dir, reactionRates_dir, configuration_dir, constraints_dir, &
                         spec_constraints_dir, env_constraints_dir, photo_constraints_dir
   logical, allocatable :: names_valid(:), values_valid(:)
@@ -225,40 +229,31 @@ PROGRAM CLI
       ! write(*,*)
       if ( names_valid(i) .neqv. .true. ) then
         all_valid = .false.
-        write(*,*) 'name number ', i, ', "', trim(names(i)), '" is not valid when paired with value "', trim(values(i)), '"'
+        write(*,*) 'supplied flag "', trim(names(i)), '" is not valid when paired with value "', trim(values(i)), '"'
       end if
       if ( values_valid(i) .neqv. .true. ) then
         all_valid = .false.
-        write(*,*) 'value number ', i, ', "', trim(values(i)), '" is not valid when paired with name "', trim(names(i)), '"'
+        write(*,*) 'value "', trim(values(i)), '" is not valid when paired with name "', trim(names(i)), '"'
       end if
       if ( flag_array_contains(valid_flags, names(i)) == 0 ) then
         all_valid = .false.
-        write(*,*) 'flag "', trim(names(i)) ,'" is not a valid flag.'
-        call print_help()
-        stop
+        write(*,*) 'supplied flag "', trim(names(i)) ,'" is not a valid flag.'
+        ! call print_help()
+        ! stop
       end if
     end do
   end if
 
   ! report back on validity
-  if (all_valid) then
-    write(*,*) 'ALL VALID - CONTINUE'
-  else
-    write(*,*) 'NOT ALL VALID'
+  if (.not. all_valid) then
     call print_help()
     stop
   end if
 
   ! check for existence of --help flag - if it exists, ignore all others, and print the help text
-  loc = array_contains(names, valid_flags(1)%flag_switch)
-
-  if ( loc /= 0 ) then
-          write(*,*) 'help called'
-          call print_help()
-          stop
-  else
-          write(*,*) 'help NOT called'
-          write(*,*) 'continue with checking that the supplied arguments are valid'
+  if ( array_contains(names, valid_flags(1)%flag_switch) /= 0 ) then
+    call print_help()
+    stop
   end if
 
   ! set each of the directory locations from the command line, following the defined logic for defaults if some are not supplied
@@ -280,4 +275,4 @@ PROGRAM CLI
   write(*,*) 'environment constraints dir: ', env_constraints_dir
   write(*,*) 'photolysis constraints dir:  ', photo_constraints_dir
 
-END PROGRAM CLI
+end program cli

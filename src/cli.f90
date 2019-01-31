f25b51b3e151973d3581b85995a6c79f2dd3c127
Sam Cox <sc676@le.ac.uk>
2019-01-31 10:20:34 +0000

    This is about there as a protoytpe - some output is to overbose currently, and it needs merging into the main code.

--- src/cli.f90
+++ src/cli.f90
@@ -1,6 +1,39 @@
 
 module helper_routines
+
+  type flag
+    character(100) :: flag_switch
+    character(200) :: flag_help
+  end type flag
+
+  type(flag), parameter :: valid_flags(9) = &
+              [ flag('--help',              'Displays this help message.'), &
+                flag('--model',             'The base directory of the model.'), &
+                flag('--output',            'The destination directory for output.'), &
+                flag('--reactionRates', 'The destination directory for the reactionRates output.'), &
+                flag('--configuration', 'The directory of the model configuration.'), &
+                flag('--constraints', 'The base directory of constraints. ' // &
+                                      'This typically contains 3 subdirectories: environment, photolysis, and species.'), &
+                flag('--spec_constraints ', 'The directory containing species constraints data.'), &
+                flag('--env_constraints  ', 'The directory containing environment constraints data.'), &
+                flag('--photo_constraints', 'The directory containing photolysis constraints data.') ]
+
 contains
+
+  subroutine print_help()
+    implicit none
+
+    integer i
+
+    write(*,*) 'This is the help message.'
+    write(*,*) 'Possible input flags are: '
+    do i=1,size(valid_flags)
+      write(*,*) trim(valid_flags(i)%flag_switch)
+      write(*,*) '     ', valid_flags(i)%flag_help
+      write(*,*)
+    end do
+    write(*,*) 'End of help message.'
+  end subroutine print_help
 ! split a string into 2 either side of a delimiter token
 SUBROUTINE split_string(instring, string1, string2, delim)
   implicit none
@@ -84,20 +117,64 @@ function array_contains(array, value) result (index)
 
   ! write(*,*) size(array)
   ! write(*,*) value
-  do i=1,size(array)
-          write(*,*) 'loop'
-          write(*,*) i, ' ', trim(array(i)), ' ', trim(value)
-          write(*,*) i, ' ', array(i), ' ', value
-    if ( trim(array(i)) == trim(value) ) then
-      index = i
-      write(*,*) 'found', i, index
-      return
-    end if
-  end do
+  if ( size(array) > 0 ) then
+    do i=1,size(array)
+      ! write(*,*) 'loop'
+      ! write(*,*) i, ' ', trim(array(i)), ' ', trim(value)
+      ! write(*,*) i, ' ', array(i), ' ', value
+      if ( trim(array(i)) == trim(value) ) then
+        index = i
+        ! write(*,*) 'found', i, index
+        return
+      end if
+    end do
+  end if
   index = 0
-  write(*,*) 'not found', index
+  ! write(*,*) 'not found', index
   return
 end function array_contains
+
+function flag_array_contains(array, value) result (index)
+  implicit none
+  type(flag), intent(in) :: array(:)
+  character(100), intent(in) :: value
+  integer :: index
+  integer :: i
+
+  ! write(*,*) size(array)
+  ! write(*,*) value
+  if ( size(array) > 0 ) then
+    do i=1,size(array)
+      ! write(*,*) 'loop'
+      ! write(*,*) i, ' ', trim(array(i)), ' ', trim(value)
+      ! write(*,*) i, ' ', array(i), ' ', value
+      if ( trim(array(i)%flag_switch) == trim(value) ) then
+        index = i
+        ! write(*,*) 'found', i, index
+        return
+      end if
+    end do
+  end if
+  index = 0
+  ! write(*,*) 'not found', index
+  return
+end function flag_array_contains
+
+function read_value_or_default( output_name, default, names, values ) result ( out )
+  implicit none
+  character(len=*), intent(in) :: output_name, default, names(:), values(:)
+  character(len=100) :: out
+  integer :: loc
+
+  loc = array_contains(names, output_name)
+
+  if ( loc /= 0 ) then
+    out = trim(values(loc))
+  else
+    out = trim(default)
+  end if
+
+end function  read_value_or_default
 
 end module helper_routines
 
@@ -119,157 +196,88 @@ PROGRAM CLI
 
   integer :: cmd_arg_count, i, loc
   character(len=100), allocatable :: input_strings(:), names(:), values(:)
+  ! character(len=100) :: valid_flags(9)
   character(len=100) :: model_dir, output_dir, reactionRates_dir, configuration_dir, constraints_dir, &
-                        spec_constraints_dir, env_constraints_dir, photo_constraints_dir, &
-                        help_name, model_name, output_name, reactionRates_name, configuration_name, &
-                        constraints_name, spec_constraints_name, env_constraints_name, photo_constraints_name
-  character(len=100) :: param_dir
+                        spec_constraints_dir, env_constraints_dir, photo_constraints_dir
   logical, allocatable :: names_valid(:), values_valid(:)
   logical :: all_valid
 
+  all_valid = .true.
+  ! count possible arguments
   cmd_arg_count = command_argument_count()
+  ! read in possible arguments
+  allocate(input_strings(cmd_arg_count), names(cmd_arg_count), values(cmd_arg_count), &
+           names_valid(cmd_arg_count), values_valid(cmd_arg_count))
+
   if ( cmd_arg_count > 0 ) then
-    allocate(input_strings(cmd_arg_count), names(cmd_arg_count), values(cmd_arg_count), &
-             names_valid(cmd_arg_count), values_valid(cmd_arg_count))
     do i=1,cmd_arg_count
       call get_command_argument( i, input_strings(i) )
     end do
-  end if
-  if ( cmd_arg_count > 0 ) then
-    call get_command_argument( 1, output_dir )
-  else
-    output_dir = "model/output"
-  end if
-  if ( cmd_arg_count > 1 ) then
-    call get_command_argument( 2, reactionRates_dir )
-  else
-    reactionRates_dir = "model/output/reactionRates"
-  end if
-  if ( cmd_arg_count > 2 ) then
-    call get_command_argument( 3, param_dir )
-  else
-    param_dir = "model/configuration"
+
+  ! parse arguments and check for naive validity
+    do i=1,cmd_arg_count
+      ! write(*,*) i, input_strings(i)
+      call split_string( input_strings(i), names(i), values(i), '=')
+      ! write(*,*) trim(names(i))
+      ! write(*,*) trim(values(i))
+      call check_name_value_pair_validity(names(i), values(i), names_valid(i), values_valid(i))
+      ! write(*,*) names_valid(i), values_valid(i)
+      ! write(*,*)
+      if ( names_valid(i) .neqv. .true. ) then
+        all_valid = .false.
+        write(*,*) 'name number ', i, ', "', trim(names(i)), '" is not valid when paired with value "', trim(values(i)), '"'
+      end if
+      if ( values_valid(i) .neqv. .true. ) then
+        all_valid = .false.
+        write(*,*) 'value number ', i, ', "', trim(values(i)), '" is not valid when paired with name "', trim(names(i)), '"'
+      end if
+      if ( flag_array_contains(valid_flags, names(i)) == 0 ) then
+        all_valid = .false.
+        write(*,*) 'flag "', trim(names(i)) ,'" is not a valid flag.'
+        call print_help()
+        stop
+      end if
+    end do
   end if
 
-  write(*,*) output_dir
-  write(*,*) reactionRates_dir
-  write(*,*) param_dir
-  do i=1,cmd_arg_count
-    write(*,*) trim(input_strings(i))
-  end do
-  write(*,*) size(input_strings)
-  write(*,*)
-  do i=1,cmd_arg_count
-    write(*,*) i, input_strings(i)
-    call split_string( input_strings(i), names(i), values(i), '=')
-    write(*,*) trim(names(i))
-    write(*,*) trim(values(i))
-    call check_name_value_pair_validity(names(i), values(i), names_valid(i), values_valid(i))
-    write(*,*) names_valid(i), values_valid(i)
-    write(*,*)
-  end do
-  all_valid = .true.
-  do i=1,cmd_arg_count
-    if ( names_valid(i) .neqv. .true. ) then
-      all_valid = .false.
-      write(*,*) 'name number ', i, ', "', trim(names(i)), '" is not valid when paired with value "', trim(values(i)), '"'
-    end if
-    if ( values_valid(i) .neqv. .true. ) then
-      all_valid = .false.
-      write(*,*) 'value number ', i, ', "', trim(values(i)), '" is not valid when paired with name "', trim(names(i)), '"'
-    end if
-  end do
+  ! report back on validity
   if (all_valid) then
     write(*,*) 'ALL VALID - CONTINUE'
   else
-    write(*,*) 'NOT ALL VALID - PRINT HELP TEXT'
+    write(*,*) 'NOT ALL VALID'
+    call print_help()
     stop
   end if
 
-  help_name = '--help'
-  model_name = '--model'
-  output_name = '--output'
-  reactionRates_name = '--reactionRates'
-  configuration_name = '--configuration'
-  constraints_name = '--constraints'
-  spec_constraints_name = '--spec_constraints'
-  env_constraints_name = '--env_constraints'
-  photo_constraints_name = '--photo_constraints'
+  ! check for existence of --help flag - if it exists, ignore all others, and print the help text
+  loc = array_contains(names, valid_flags(1)%flag_switch)
 
-  loc = array_contains(names, help_name)
-  write(*,*) loc
   if ( loc /= 0 ) then
           write(*,*) 'help called'
-          write(*,*) "HERE IS THE HELP TEXT: NOW I'M GOING TO QUIT"
+          call print_help()
           stop
   else
           write(*,*) 'help NOT called'
           write(*,*) 'continue with checking that the supplied arguments are valid'
   end if
-  loc = array_contains(names, model_name)
-  write(*,*) loc
-  if ( loc /= 0 ) then
-          model_dir = values(loc)
-  else
-          model_dir = 'model'
-  end if
-  loc = array_contains(names, output_name)
-  write(*,*) loc
-  if ( loc /= 0 ) then
-          output_dir = values(loc)
-  else
-          output_dir = trim(model_dir)//'/output'
-  end if
-  loc = array_contains(names, reactionRates_name)
-  write(*,*) loc
-  if ( loc /= 0 ) then
-          reactionRates_dir = values(loc)
-  else
-          reactionRates_dir = trim(output_dir)//'/reactionRates'
-  end if
-  loc = array_contains(names, configuration_name)
-  write(*,*) loc
-  if ( loc /= 0 ) then
-          configuration_dir = values(loc)
-  else
-          configuration_dir = trim(model_dir)//'/configuration'
-  end if
-  loc = array_contains(names, constraints_name)
-  write(*,*) loc
-  if ( loc /= 0 ) then
-          constraints_dir = values(loc)
-  else
-          constraints_dir = trim(model_dir)//'/constraints'
-  end if
-  loc = array_contains(names, spec_constraints_name)
-  write(*,*) loc
-  if ( loc /= 0 ) then
-          spec_constraints_dir = values(loc)
-  else
-          spec_constraints_dir = trim(constraints_dir)//'/species'
-  end if
-  loc = array_contains(names, env_constraints_name)
-  write(*,*) loc
-  if ( loc /= 0 ) then
-          env_constraints_dir = values(loc)
-  else
-          env_constraints_dir = trim(constraints_dir)//'/environment'
-  end if
-  loc = array_contains(names, photo_constraints_name)
-  write(*,*) loc
-  if ( loc /= 0 ) then
-          photo_constraints_dir = values(loc)
-  else
-          photo_constraints_dir = trim(constraints_dir)//'/photolysis'
-  end if
 
-  write(*,*) 'model dir:                  ', model_dir
-  write(*,*) 'output dir:                 ', output_dir
-  write(*,*) 'reactionRates dir:          ', reactionRates_dir
-  write(*,*) 'configuration dir:          ', configuration_dir
-  write(*,*) 'constraints dir:            ', constraints_dir
-  write(*,*) 'species constraints dir:    ', spec_constraints_dir
-  write(*,*) 'environment constraints dir:', env_constraints_dir
-  write(*,*) 'photolysis constraints dir: ', photo_constraints_dir
+  ! set each of the directory locations from the command line, following the defined logic for defaults if some are not supplied
+  model_dir             = read_value_or_default( valid_flags(2)%flag_switch, 'model',                               names, values )
+  output_dir            = read_value_or_default( valid_flags(3)%flag_switch, trim(model_dir)//'/output',            names, values )
+  reactionRates_dir     = read_value_or_default( valid_flags(4)%flag_switch, trim(output_dir)//'/reactionRates',    names, values )
+  configuration_dir     = read_value_or_default( valid_flags(5)%flag_switch, trim(model_dir)//'/configuration',     names, values )
+  constraints_dir       = read_value_or_default( valid_flags(6)%flag_switch, trim(model_dir)//'/constraints',       names, values )
+  spec_constraints_dir  = read_value_or_default( valid_flags(7)%flag_switch, trim(constraints_dir)//'/species',     names, values )
+  env_constraints_dir   = read_value_or_default( valid_flags(8)%flag_switch, trim(constraints_dir)//'/environment', names, values )
+  photo_constraints_dir = read_value_or_default( valid_flags(9)%flag_switch, trim(constraints_dir)//'/photolysis',  names, values )
+
+  write(*,*) 'model dir:                   ', model_dir
+  write(*,*) 'output dir:                  ', output_dir
+  write(*,*) 'reactionRates dir:           ', reactionRates_dir
+  write(*,*) 'configuration dir:           ', configuration_dir
+  write(*,*) 'constraints dir:             ', constraints_dir
+  write(*,*) 'species constraints dir:     ', spec_constraints_dir
+  write(*,*) 'environment constraints dir: ', env_constraints_dir
+  write(*,*) 'photolysis constraints dir:  ', photo_constraints_dir
 
 END PROGRAM CLI

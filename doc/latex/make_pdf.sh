bc4cd2cc9621ffc42bd3500009f967f741a118ae
R. Sommariva <rob.sommariva@gmail.com>
2019-05-19 20:29:54 +0100

    Organize documentation (#393)
    * Move latex files to separate directory
    
    * Add poster from the ACM conference
    
    * Include reference to pdf file
    
    * Start cleaning latex code
    
    * Script to compile manual pdf
    
    * Consolidate sections in chapters
    
    * Keep latex log and aux files
    
    * Tidying sections and labels
    
    * Fix and Tidy Latex source
    
    * Format Latex source
    
    * Fix internal links and split Setup.tex
    
    * Add copyright header
    
    * Formatting latex source
    
    * Update pdf
    
    * Add instructions for argument parser

---
+++ doc/latex/make_pdf.sh
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2017 Sam Cox, Roberto Sommariva
+#
+# This file is part of the AtChem2 software package.
+#
+# This file is covered by the MIT license which can be found in the file
+# LICENSE.md at the top level of the AtChem2 distribution.
+#
+# -----------------------------------------------------------------------------
+
+#!/bin/sh
+#
+# Compile the LaTeX source files of the AtChem2 manual into a pdf file
+
+pdflatex AtChem2-Manual.tex
+pdflatex AtChem2-Manual.tex
+
+mv -f AtChem2-Manual.pdf ../AtChem2-Manual.pdf

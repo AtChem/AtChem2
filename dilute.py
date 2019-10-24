cb27b8be8682307589e98b34ec87b98c58e47810
BS Nelson <bsn502@login1.pri.viking.alces.network>
2019-10-24 14:12:28 +0100

    Fixed new lines characters

--- dilute.py
+++ dilute.py
@@ -1,34 +1,68 @@
-import sys
-environmentVariables = open("./model/configuration/environmentVariables.config").readlines()
-mech_file = str(sys.argv[1])
-
-for x in environmentVariables: print(x)
-
-#file[0]
-
-for x in environmentVariables:
-    x = x.split()
-    
-    try:
-        if x[1] == "DILUTE" :
-            y = (x[2])
-    
-    except IndexError:
-        continue
-
-if y == "NOTUSED" :
-    sys.exit()
-
-        
-spec = open("./model/configuration/mechanism.species").readlines()
-
-f = open(f"./{mech_file}","a+")
-
-for x in spec:
-    x = x.split()
-    z = (x[1])
-    f.write(f"% {y} : {z} = LOSS ;\n")
-        
-f.close()
-
-
+import sys
+
+environmentVariables = open("./model/configuration/environmentVariables.config").readlines()
+
+mech_file = str(sys.argv[1])
+
+
+
+for x in environmentVariables: print(x)
+
+
+
+#file[0]
+
+
+
+for x in environmentVariables:
+
+    x = x.split()
+
+    
+
+    try:
+
+        if x[1] == "DILUTE" :
+
+            y = (x[2])
+
+    
+
+    except IndexError:
+
+        continue
+
+
+
+if y == "NOTUSED" :
+
+    sys.exit()
+
+
+
+        
+
+spec = open("./model/configuration/mechanism.species").readlines()
+
+
+
+f = open(f"./{mech_file}","a+")
+
+
+
+for x in spec:
+
+    x = x.split()
+
+    z = (x[1])
+
+    f.write(f"% {y} : {z} = LOSS ;\n")
+
+        
+
+f.close()
+
+
+
+
+

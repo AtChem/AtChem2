bb15f8497682cf75954d46ab260b3c1ca89832bc
BS Nelson <bsn502@login1.pri.viking.alces.network>
2019-10-25 15:14:34 +0100

    Changes made to make script python 2.7 compatible

--- dilute.py
+++ dilute.py
@@ -1,68 +1,29 @@
 import sys
-
 environmentVariables = open("./model/configuration/environmentVariables.config").readlines()
-
 mech_file = str(sys.argv[1])
-
-
 
 for x in environmentVariables: print(x)
 
-
-
 #file[0]
 
-
-
 for x in environmentVariables:
-
     x = x.split()
-
     
-
     try:
-
         if x[1] == "DILUTE" :
-
             y = (x[2])
-
     
-
     except IndexError:
-
         continue
 
-
-
 if y == "NOTUSED" :
-
     sys.exit()
 
-
-
         
-
 spec = open("./model/configuration/mechanism.species").readlines()
-
-
-
-f = open(f"./{mech_file}","a+")
-
-
-
-for x in spec:
-
-    x = x.split()
-
-    z = (x[1])
-
-    f.write(f"% {y} : {z} = LOSS ;\n")
-
-        
-
-f.close()
-
-
-
-
-
+ 
+with open(mech_file,"a+") as f:
+    for x in spec:
+        x = x.split()
+        z = (x[1])
+        f.write('% {} : {} = LOSS ;\n'.format(str(y),str(z)))

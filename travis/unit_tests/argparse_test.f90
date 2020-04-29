a56ca8cc90b541139de81088fccf967040992fa0
rs028 <rob.sommariva@gmail.com>
2020-04-29 20:20:58 +0100

    Add copyright note to unit tests

--- travis/unit_tests/argparse_test.f90
+++ travis/unit_tests/argparse_test.f90
@@ -1,3 +1,14 @@
+! -----------------------------------------------------------------------------
+!
+! Copyright (c) 2017 Sam Cox, Roberto Sommariva
+!
+! This file is part of the AtChem2 software package.
+!
+! This file is covered by the MIT license which can be found in the file
+! LICENSE.md at the top level of the AtChem2 distribution.
+!
+! -----------------------------------------------------------------------------
+
 module argparse_test
   use fruit
   use types_mod

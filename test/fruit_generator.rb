e6258ba76e05b1881200e16bab1e04c7940b72b6
Sam Cox <spco@users.noreply.github.com>
2021-02-01 11:32:43 +0000

    Implement CI on GH Actions (#441)
    Moving from Travis CI to GH Actions. We want to test against gfortran v8 and v9 on MacOs and Linux. This requires the following:
    
    * Create .github/workflows/ci.yml to control Actions.
    
    * Rename travis/ directory to test/, and alter all .cmp files to reference this directory. The only change not yet made is in the Installation.tex, lines 395 and 402, and the rebuild of the manual. Also edit .codecov.yml (moves to .github/workflows/) to reflect this.
    
    * Use GH Actions environment variables in Makefile. Set FORT_COMP to the correct location on macOS on Actions.
    
    * Move setting of LIBDIR outside of the ifeq gfortran condition in Makefile.skel to avoid having to match the exact name of the gfortran compiler executable.
    
    * Remove a FORT_LIB line from Makefile.skel which won't be used.
    
    * Edit a few lines in travis/fruit_generator.rb to use the correct environment variables set within GH Actions.
    
    * Remove .travis.yml.
    
    * Change README.md CI badge to Actions.
    
    Additionally, in this commit we update the openlibm URL to new location as it has permanently moved.

---
+++ test/fruit_generator.rb
@@ -0,0 +1,37 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2017 Sam Cox, Roberto Sommariva
+#
+# This file is part of the AtChem2 software package.
+#
+# This file is covered by the MIT license which can be found in the file
+# LICENSE.md at the top level of the AtChem2 distribution.
+#
+# -----------------------------------------------------------------------------
+
+# This script creates a new FruitProcessor object, and generates files
+# to control the unit tests.
+require 'rubygems'
+require 'fruit_processor'
+
+if ENV['GITHUB_ACTIONS'] == nil
+  load "fruit_3.4.3/rake_base.rb"
+else
+  if ENV['RUNNER_OS'] == 'Linux'
+    load "/home/runner/work/AtChem2/AtChem2/fruit_3.4.3/rake_base.rb"
+  else
+    load "/Users/runner/work/AtChem2/AtChem2/fruit_3.4.3/rake_base.rb"
+  end
+end
+
+# fruit.f90 and fruit_util.f90 must exist in this directry.
+$build_dir = ""  # if not set, build will be done in ../build/
+# $source_dirs = ["../src", "./"]  # "../src" is for fruit.f90 and fruit_util.f90
+$goal = "fruit_driver_dummy.exe"
+
+# Add this line at the beginning if there are generated code involved
+fp = FruitProcessor.new
+#fp.shuffle = true          # if true order of methods will be random
+fp.pre_process              # generates fruit_*_gen.f90
+
+#eof

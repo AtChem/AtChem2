aeaf3f381b347fe92a715a3096b81daeaf9b0d1a
rs028 <rob.sommariva@gmail.com>
2023-03-24 15:24:30 +0000

    Fix matlab script

--- tools/plot/plot-atchem2.m
+++ tools/plot/plot-atchem2.m
@@ -22,10 +22,25 @@ arg_list = argv();
 cd(arg_list{1});
 pwd
 
-df1 = readtable('speciesConcentrations.output');
-df2 = readtable('environmentVariables.output');
-df3 = readtable('photolysisRates.output');
-df4 = readtable('photolysisRatesParameters.output');
+fin = fopen('speciesConcentrations.output','r');
+var1 = strsplit(fgetl(fin), ' ');
+df1 = dlmread ('speciesConcentrations.output', '', 1, 0);
+fclose(fin);
+
+fin = fopen('environmentVariables.output','r');
+var2 = strsplit(fgetl(fin), ' ');
+df2 = dlmread ('environmentVariables.output', '', 1, 0);
+fclose(fin);
+
+fin = fopen('photolysisRates.output','r');
+var3 = strsplit(fgetl(fin), ' ');
+df3 = dlmread ('photolysisRates.output', '', 1, 0);
+fclose(fin);
+
+fin = fopen('photolysisRatesParameters.output','r');
+var4 = strsplit(fgetl(fin), ' ');
+df4 = dlmread ('photolysisRatesParameters.output', '', 1, 0);
+fclose(fin);
 
 nc1 = size(df1, 2);
 nc2 = size(df2, 2);
@@ -34,39 +49,52 @@ nc4 = size(df4, 2);
 
 %% ---------------------------- %%
 
+figure('PaperSize', [11 7], 'visible', 'off');
+pdf_file = 'atchem2_output.pdf';
+
 %% speciesConcentrations.output
 for i = 2:nc1
-    subplot(3,3,i-1)
-    plot(df1{:,1}, df1{:,i}, '-k')
-    title(df1.Properties.VariableNames{i})
-    xlabel('seconds'), ylabel('')
+    j = mod(i-2,9);
+    subplot(3,3,j+1);
+    plot(df1(:,1), df1(:,i), '-k');
+    title(var1{i+1}), xlabel('seconds'), ylabel('');
+    if j == 0 && i < nc1
+        print(pdf_file, '-dpdf', '-fillpage', '-append');
+    end
 end
 
 %% environmentVariables.output
 for i = 2:nc2
-    subplot(3,3,i-1)
-    plot(df2{:,1}, df2{:,i}, '-k')
-    title(df2.Properties.VariableNames{i})
-    xlabel('seconds'), ylabel('')
+    j = mod(i-2,9);
+    subplot(3,3,j+1);
+    plot(df2(:,1), df2(:,i), '-k');
+    title(var2{i+1}), xlabel('seconds'), ylabel('');
+    if j == 0 && i < nc2
+        print(pdf_file, '-dpdf', '-fillpage', '-append');
+    end
 end
 
 %% photolysisRates.output
 for i = 2:nc3
-    subplot(3,3,i-1)
-    plot(df3{:,1}, df3{:,i}, '-k')
-    title(df3.Properties.VariableNames{i})
-    xlabel('seconds'), ylabel('')
+    j = mod(i-2,9);
+    subplot(3,3,j+1);
+    plot(df3(:,1), df3(:,i), '-k');
+    title(var3{i+1}), xlabel('seconds'), ylabel('');
+    if j == 0 && i < nc3
+        print(pdf_file, '-dpdf', '-fillpage', '-append');
+    end
 end
 
 %% photolysisRatesParameters.output
 for i = 2:nc4
-    subplot(3,3,i-1)
-    plot(df4{:,1}, df4{:,i}, '-k')
-    title(df4.Properties.VariableNames{i})
-    xlabel('seconds'), ylabel('')
+    j = mod(i-2,9);
+    subplot(3,3,j+1);
+    plot(df4(:,1), df4(:,i), '-k');
+    title(var4{i+1}), xlabel('seconds'), ylabel('');
+    if j == 0 && i < nc4
+        print(pdf_file, '-dpdf', '-fillpage', '-append');
+    end
 end
-
-print('atchem2_output.pdf', '-dpdf', '-S1100,700')
 
 %% ---------------------------- %%
 

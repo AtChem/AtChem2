ae1d3c67397e53da667f6f8a6cbcd62a975872fb
rs028 <rob.sommariva@gmail.com>
2023-03-27 19:01:49 +0100

    Fix matlab script

--- tools/plot/plot-atchem2.m
+++ tools/plot/plot-atchem2.m
@@ -18,9 +18,12 @@
 %% USAGE:
 %%   octave ./tools/plot/plot-atchem2.m ./model/output/
 %% ---------------------------------------------- %%
-arg_list = argv();
-cd(arg_list{1});
-pwd
+if length(argv) < 1
+    error('Please provide the model output directory as an argument.');
+else
+    output_dir = argv{1};
+    cd(output_dir);
+end
 
 fin = fopen('speciesConcentrations.output','r');
 var1 = strsplit(fgetl(fin), ' ');
@@ -49,53 +52,69 @@ nc4 = size(df4, 2);
 
 %% ---------------------------- %%
 
-figure('PaperSize', [11 7], 'visible', 'off');
 pdf_file = 'atchem2_output.pdf';
+if exist(pdf_file, 'file') == 2
+    delete(pdf_file);
+end
+
+figure('PaperSize', [11 7], 'visible', 'off');
 
 %% speciesConcentrations.output
-for i = 2:nc1
-    j = mod(i-2,9);
-    subplot(3,3,j+1);
+j = 1;
+for i = 2:1:nc1
+    subplot(3,3,j);
     plot(df1(:,1), df1(:,i), '-k');
     title(var1{i+1}), xlabel('seconds'), ylabel('');
-    if j == 0 && i < nc1
+    if (j == 9 || i == nc1)
         print(pdf_file, '-dpdf', '-fillpage', '-append');
+        j = 1; clf;
+    else
+        j = j + 1;
     end
 end
 
 %% environmentVariables.output
-for i = 2:nc2
-    j = mod(i-2,9);
-    subplot(3,3,j+1);
+j = 1;
+for i = 2:1:nc2
+    subplot(3,3,j);
     plot(df2(:,1), df2(:,i), '-k');
     title(var2{i+1}), xlabel('seconds'), ylabel('');
-    if j == 0 && i < nc2
+    if (j == 9 || i == nc2)
         print(pdf_file, '-dpdf', '-fillpage', '-append');
+        j = 1; clf;
+    else
+        j = j + 1;
     end
 end
 
 %% photolysisRates.output
-for i = 2:nc3
-    j = mod(i-2,9);
-    subplot(3,3,j+1);
+j = 1;
+for i = 2:1:nc3
+    subplot(3,3,j);
     plot(df3(:,1), df3(:,i), '-k');
     title(var3{i+1}), xlabel('seconds'), ylabel('');
-    if j == 0 && i < nc3
+    if (j == 9 || i == nc3)
         print(pdf_file, '-dpdf', '-fillpage', '-append');
+        j = 1; clf;
+    else
+        j = j + 1;
     end
 end
 
 %% photolysisRatesParameters.output
-for i = 2:nc4
-    j = mod(i-2,9);
-    subplot(3,3,j+1);
+j = 1;
+for i = 2:1:nc4
+    subplot(3,3,j);
     plot(df4(:,1), df4(:,i), '-k');
     title(var4{i+1}), xlabel('seconds'), ylabel('');
-    if j == 0 && i < nc4
+    if (j == 9 || i == nc4)
         print(pdf_file, '-dpdf', '-fillpage', '-append');
+        j = 1; clf;
+    else
+        j = j + 1;
     end
 end
 
 %% ---------------------------- %%
 
-fprintf('\n==> atchem2_output.pdf created in directory: %s\n\n', arg_list{1});
+fprintf('\n==> atchem2_output.pdf created in directory: %s\n\n', output_dir);

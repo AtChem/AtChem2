b47d0daccda2c821d95700dcd92dfaa8e427b91e
rs028 <rob.sommariva@gmail.com>
2023-04-18 19:36:22 +0100

    Tidy fortran checking scripts

--- tools/fix_indent.py
+++ tools/fix_indent.py
@@ -9,39 +9,55 @@
 #
 # -----------------------------------------------------------------------------
 
-# This script indents the source file provided as an argument, replacing it with a copy that better fits the
-# expected style. If two arguments are given, the output wil be written to the second, leaving the first untouched.
+# -------------------------------------------------------------------- #
+# This script rewrites the Fortran source file provided as an argument,
+# replacing it with a copy that better fits the expected style (with
+# regard to code indentation).
+# If two arguments are given, the output will be written to the second,
+# leaving the first untouched.
 #
-# WARNING: Please note that this script is not infallible, and could break your code! Please use with caution,
-# and make sure you have a copy of your source file to revert to in the event of it breaking.
-#
-# The workflow of this script is quite simple. For each line:
+# WARNING: this script is not infallible, and can break your code!
+# Please use with caution, and make sure you have a copy of your
+# source file to revert to in the event of it breaking.
+# -------------------------------------------------------------------- #
+# The workflow of this script is straightforward. For each line in the
+# Fortran source file:
 #  * strip off the trailing newline
-#  * strip out the comment from the end of the line, relying on the fact that quotes
-#    are not able to span lines, to ensure we really grab a '!' marking the comment start, and not in a string literal.
-#  * if the non-comment content is all whitespace, then skip to reappending the comment, otherwise:
-#  * if the previous line was e.g. the start of a procedure, then increase the indent on this line.
-#  * store whether this line ends in an ampersand
-#  * if the previous line ended in an ampersand, then we don't know where to align to, so leave untouched.
-#    * otherwise, 'end', 'else' or 'contains' means this line should be unindented.
-#    * if this line is the start of e.g. a procedure, store that the next line (that isn't following an ampersand) should be indented.
+#  * strip out the comment from the end of the line, relying on the fact that
+#    quotes cannot span lines, to ensure we really grab a '!' marking the
+#    comment start, and not one inside a string literal.
+#  * if the non-comment content is all whitespace, then skip to reappending the
+#    comment, otherwise:
+#    * if the previous line was e.g. the start of a procedure, then increase the
+#      indent on this line.
+#    * store whether this line ends in an ampersand
+#    * if the previous line ended in an ampersand, then we don't know where to
+#      align to, so leave untouched.
+#    * otherwise, the presence of 'end', 'else' or 'contains' means that this
+#      line should be unindented.
+#    * if this line is the start of e.g. a procedure, store that the next line
+#      (that isn't following an ampersand) should be indented.
 #  * perform appropriate indentation, rejoin comment and newline, and output.
-#  * set up variables for next loop, passing on indent setting and whether this was an ampersand-ending line.
+#  * set up variables for next loop, passing on indent setting and whether this
+#    line ended with an ampersand.
+# -------------------------------------------------------------------- #
 from __future__ import print_function
+import sys, re
 
-## ------------------------------------------------------------------ ##
+## ------------------------------------------------------------ ##
 
-# strip newline characters from string
+# Strip newline characters from string
 def strip_newline(string):
     string = re.sub('\n', '', string)
     return string
 
-# append newline character to string
+# Append newline character to string
 def add_newline(string):
     string = string + '\n'
     return string
 
-# count whether there are an even number of both single- and double-quotes in the given string.
+# Check that there are an even number of both single- and
+# double-quotes in the given string
 def even_quotes(string):
     if string.count('"') % 2 == 0:
         double = True
@@ -53,12 +69,10 @@ def even_quotes(string):
         single = False
     return (double and single)
 
-## ------------------------------------------------------------------ ##
+## ------------------------------------------------------------ ##
 
-import sys
-import re
-
-# handle input arguments. If only one is provided, use this for input and output. Error if none provided.
+# Handle input arguments. If only one is provided, use this for both
+# input and output. Error if none provided.
 assert len(sys.argv) >= 2, "Please enter a filename as argument."
 filename = sys.argv[1]
 if len(sys.argv) == 3:
@@ -66,13 +80,13 @@ if len(sys.argv) == 3:
 else:
     out_filename = filename
 
-# read in file contents
+# Read in file contents
 with open(filename, 'r') as input_file:
     lines = input_file.readlines()
 
-# loop for indenting
+# Loop for fixing indent
 with open(out_filename, 'w') as output_file:
-    # set up variables for first time
+    # Set up variables for first time
     previous_line_ends_ampersand = False
     this_line_ends_ampersand = False
     next_line_indent_more = False
@@ -82,12 +96,12 @@ with open(out_filename, 'w') as output_file:
     outputs = []
     for line in lines:
         empty_line = False
-        # extract comment from end of line, to append later
+        # Extract comment from end of line, to append later
         line = strip_newline(line)
         split_line = line.split('!')
         to_output = split_line[0]
         if len(split_line) > 1:
-            # cycle over each combo of strings until we split command from comment
+            # Cycle over each combo of strings until we split command from comment
             for i in range(len(split_line)):
                 to_output = '!'.join(split_line[0:i+1])
                 if even_quotes(to_output):
@@ -99,31 +113,35 @@ with open(out_filename, 'w') as output_file:
         else:
             comment = ''
 
+        # Check that the string contains only whitespace or is empty
         if to_output.isspace() or to_output == '':
             empty_line = True
 
+        # Keep track of the indentation level
         if next_line_indent_more:
             indent = indent+1
             next_line_indent_more = False
 
+        # Check that this line ends with ampersand
         if not empty_line:
             this_line_ends_ampersand = False
-            # check whether this line ends in ampersand
             if re.search('&\s*$', to_output):
                 this_line_ends_ampersand = True
 
+            # This line starts with 'end', so the next line should be unindented
             if not previous_line_ends_ampersand:
-                # check if this line starts with 'end' : next line should be unindented
                 if re.match('^\s*end\s*', to_output, flags=re.IGNORECASE) \
                   or re.match('^\s*else\s*', to_output, flags=re.IGNORECASE) \
                   or re.match('^\s*contains\s*', to_output, flags=re.IGNORECASE):
                     indent = indent-1
 
-                # Handle the fact that each case of a select structure doesn't end in an 'end', so the 'end selec' needs to go back twice.
+                # Handle the fact that each case of a select structure doesn't end in an 'end',
+                # so the 'end select' needs to go back twice
                 if re.match('^\s*end select\s*', to_output, flags=re.IGNORECASE):
                     indent = indent - 1
 
-                # match if, do, subroutine, function, module, else, contains, program, interface, select, case, type (definition, not instantiation) to set the next line indent higher
+                # Match if-then, do, subroutine, function, module, else, contains, program, interface,
+                # select, case, type (definition, not instantiation) to set the next line indent higher
                 if re.search('\s*IF.+THEN', to_output, flags=re.IGNORECASE) or re.match('^\s*else\s*', to_output, flags=re.IGNORECASE) or re.match('^\s*do\s*', to_output, flags=re.IGNORECASE) \
                     or re.match('^\s*subroutine\s*', to_output, flags=re.IGNORECASE) or re.match('^\s*function\s*', to_output, flags=re.IGNORECASE) \
                     or ( re.match('^\s*module\s*', to_output, flags=re.IGNORECASE) and not re.match('^\s*module procedure \s*', to_output, flags=re.IGNORECASE) ) \
@@ -134,12 +152,12 @@ with open(out_filename, 'w') as output_file:
                     or re.match('^\s*case\s*', to_output, flags=re.IGNORECASE) or re.match('^\s*type\s+', to_output, flags=re.IGNORECASE):
                     next_line_indent_more = True
 
-                # set start_select when we enter a select structure
+                # Set start_select when we enter a 'select' structure
                 if re.match('^\s*select\s*', to_output, flags=re.IGNORECASE):
                     start_select = True
 
-                # if at a 'case' statement, check whether it's the first one, via start_select
-                # if so, don't change the indent, as we just want it to be indented next time
+                # If at a 'case' statement, check whether it's the first one, via start_select.
+                # If so, don't change the indent, as we just want it to be indented next time;
                 # otherwise, unindent by one
                 if re.match('^\s*case\s*', to_output, flags=re.IGNORECASE):
                     if start_select:
@@ -147,22 +165,29 @@ with open(out_filename, 'w') as output_file:
                     else:
                         indent = indent-1
 
+            # This line does not start with 'end'
             else:
                 next_line_indent_more = False
 
+        # Check that the previous line does not end with ampersand,
+        # then add correct indentation
         if not previous_line_ends_ampersand:
             if re.search('\S', to_output):
                 to_output = re.sub('^\s*(?=\S)', ' '*2*indent, to_output)
-            elif re.search('\S', to_output + comment):
+            elif re.search('\S', to_output+comment):
                 to_output = ''
                 comment = (' '*2*indent)+comment
 
+        # Update previous_line_ends_ampersand, based on whether this
+        # line ends with an ampersand
         if this_line_ends_ampersand:
             previous_line_ends_ampersand = True
         else:
             previous_line_ends_ampersand = False
 
+        # add amended line to output
         outputs.append(to_output + add_newline(comment))
 
+    # write output to file
     output_file.writelines(outputs)
     print('Complete! Now run a find and replace by hand with regex "&\s*\\n" to catch alignment of the lines following ampersands.')

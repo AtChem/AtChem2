f12943c03fb08597984248b53e08eb074d33e5ff
rs028 <rob.sommariva@gmail.com>
2023-09-06 12:53:16 +0100

    All unit tests

---
+++ tests/unit_tests/config_test.f90
@@ -0,0 +1,138 @@
+! -----------------------------------------------------------------------------
+!
+! Copyright (c) 2017 Sam Cox, Roberto Sommariva
+!
+! This file is part of the AtChem2 software package.
+!
+! This file is covered by the MIT license which can be found in the file
+! LICENSE.md at the top level of the AtChem2 distribution.
+!
+! -----------------------------------------------------------------------------
+
+module config_test
+  use fruit
+  use types_mod
+  implicit none
+
+contains
+
+  subroutine test_matchNameToNumber
+    use types_mod
+    use config_functions_mod, only : matchNameToNumber
+    use storage_mod, only : maxSpecLength
+
+    character(len=maxSpecLength) :: masterSpeciesList(10), testSpeciesList(6)
+    character(len=35) :: testNumber
+    integer(kind=NPI) :: returnArray(6), answerArray(6), i
+
+    masterSpeciesList = (/ ' CO', 'NO2', ' O3', ' O2', 'H2O', ' OH', 'CO2', ' C6', '  K', ' Pb' /)
+    testSpeciesList = (/ ' O3', 'NO2', ' CO', ' C6', 'H2O', ' Pb' /)
+
+    answerArray = (/ 3, 2, 1, 8, 5, 10/)
+
+    call matchNameToNumber( masterSpeciesList, testSpeciesList, returnArray )
+    ! loop over each element, checking that equality with answerArray holds
+    do i = 1, 6
+        write (testNumber,'(A31, I1)') "test_matchNameToNumber element ", i
+        call assert_true( returnArray(i) == answerArray(i), testNumber )
+    enddo
+  end subroutine test_matchNameToNumber
+
+  subroutine test_getIndexWithinList
+    use types_mod
+    use config_functions_mod, only : getIndexWithinList
+
+    character(len=2) :: list(5)
+
+    list = (/ 'a', 'b', 'c', 'd', 'e' /)
+
+    call assert_true(getIndexWithinList( list, 'a' ) == 1_NPI, "getIndexWithinList, a = 1")
+    call assert_true(getIndexWithinList( list, 'e' ) == 5_NPI, "getIndexWithinList, e = 5")
+    ! Any items not in the list should return 0
+    call assert_true(getIndexWithinList( list, 'f' ) == 0_NPI, "getIndexWithinList, f = 0")
+    call assert_true(getIndexWithinList( list, 'z' ) == 0_NPI, "getIndexWithinList, z = 0")
+    call assert_true(getIndexWithinList( list, ' 0' ) == 0_NPI, "getIndexWithinList,  0 = 0")
+  end subroutine test_getIndexWithinList
+
+  subroutine test_findReactionsWithProductOrReactant
+    use types_mod
+    use config_functions_mod, only : findReactionsWithProductOrReactant
+
+    integer(kind=NPI) :: speciesList(3), chs(2, 7), lens(3), i, j, lenAnswer(3)
+    type(reaction_frequency_pair) :: r(3, 10), rAnswer(3, 10)
+
+    do i = 1, size( r, 1 )
+      do j = 1, size( r, 2 )
+        r(i,j)%reaction = 0_NPI
+        rAnswer(i, j)%reaction = 0_NPI
+        r(i,j)%frequency = 0_NPI
+        rAnswer(i, j)%frequency = 0_NPI
+      enddo
+    enddo
+    speciesList = (/ 3_NPI, 2_NPI, 1_NPI /)
+    !      reaction number, species number
+    chs = reshape((/ 1_NPI, 3_NPI, &
+                     1_NPI, 4_NPI, &
+                     2_NPI, 1_NPI, &
+                     2_NPI, 2_NPI, &
+                     2_NPI, 2_NPI, &
+                     2_NPI, 3_NPI, &
+                     3_NPI, 2_NPI /), (/ size( chs, 1 ), size( chs, 2 )/))
+    lenAnswer = (/ 2_NPI, 2_NPI, 1_NPI /)
+    ! species 3 occurs in reaction 1 once
+    rAnswer(1,1)%reaction = 1_NPI
+    rAnswer(1,1)%frequency = 1_NPI
+    ! species 3 occurs in reaction 2 once
+    rAnswer(1,2)%reaction = 2_NPI
+    rAnswer(1,2)%frequency = 1_NPI
+    ! species 2 occurs in reaction 2 twice
+    rAnswer(2,1)%reaction = 2_NPI
+    rAnswer(2,1)%frequency = 2_NPI
+    ! species 2 occurs in reaction 3 once
+    rAnswer(2,2)%reaction = 3_NPI
+    rAnswer(2,2)%frequency = 1_NPI
+    ! species 1 occurs in reaction 2 once
+    rAnswer(3,1)%reaction = 2_NPI
+    rAnswer(3,1)%frequency = 1_NPI
+
+    call findReactionsWithProductOrReactant(speciesList,chs,r,lens)
+    do i = 1_NPI, size( lenAnswer )
+      call assert_true( lenAnswer(i) == lens(i), "test_findReactionsWithProductOrReactant length" )
+    enddo
+
+    do i = 1_NPI, size( r, 1 )
+      do j = 1_NPI, size( r, 2 )
+        call assert_true( rAnswer(i,j) == r(i,j), "test_findReactionsWithProductOrReactant r" )
+      enddo
+    enddo
+  end subroutine test_findReactionsWithProductOrReactant
+
+  subroutine test_getSubsetOfConcs
+    use types_mod
+    use config_functions_mod, only : getSubsetOfConcs
+    use species_mod, only : setSpeciesList, setNumberOfSpecies
+    use storage_mod, only : maxSpecLength
+
+    character(len=maxSpecLength) :: fullListOfSpecies(10), speciesSubset(6)
+    real(kind=DP) :: fullListOfConcs(10), concsSubset(6), concsAnswer(6)
+    integer(kind=NPI) :: i
+
+    ! Set up the environment of species
+    fullListOfSpecies = (/ ' CO', 'NO2', ' O3', ' O2', 'H2O', ' OH', 'CO2', ' C6', '  K', ' Pb' /)
+    call setNumberOfSpecies( 10_NPI )
+    call setSpeciesList( fullListOfSpecies )
+
+    ! Pass in the full list of cncentrations, and those we're interest in
+    fullListOfConcs = (/ 0.1_DP, 0.2_DP, 0.5_DP, 0.7_DP, 1.0_DP, 1.3_DP, 1.5_DP, 1.7_DP, 1.9_DP, 2.0_DP /)
+    speciesSubset = (/ ' O3', 'NO2', ' CO', ' C6', 'H2O', ' Pb' /)
+    concsSubset = getSubsetOfConcs( fullListOfConcs, speciesSubset )
+
+    ! The function should return the subset of concentrations for the given species
+    concsAnswer = (/ 0.5_DP, 0.2_DP, 0.1_DP, 1.7_DP, 1.0_DP, 2.0_DP /)
+
+    do i = 1_NPI, size( speciesSubset )
+      call assert_true( concsSubset(i) == concsAnswer(i), "test_getSubsetOfConcs" )
+    enddo
+  end subroutine test_getSubsetOfConcs
+
+end module config_test
---
+++ tests/unit_tests/date_test.f90
@@ -0,0 +1,248 @@
+! -----------------------------------------------------------------------------
+!
+! Copyright (c) 2017 Sam Cox, Roberto Sommariva
+!
+! This file is part of the AtChem2 software package.
+!
+! This file is covered by the MIT license which can be found in the file
+! LICENSE.md at the top level of the AtChem2 distribution.
+!
+! -----------------------------------------------------------------------------
+
+module date_test
+  use fruit
+  use types_mod
+  use date_mod
+  implicit none
+
+contains
+
+  subroutine test_isLeapYear
+    use types_mod
+
+    call assert_true( isLeapYear(2004_DI) ,  "2004 is a leap year" )
+    call assert_true( isLeapYear(2000_DI) ,  "2000 is a leap year" )
+    call assert_true( isLeapYear(1996_DI) ,  "1996 is a leap year" )
+    call assert_false( isLeapYear(2001_DI) , "2001 is not a leap year" )
+    call assert_true( isLeapYear(1600_DI) ,  "1600 is a leap year" )
+    call assert_false( isLeapYear(1900_DI) , "1900 is not a leap year" )
+  end subroutine test_isLeapYear
+
+  subroutine test_applyLeapDay
+    use types_mod
+    integer(kind=DI) :: monthList(12)
+
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2004_DI)
+    call assert_true( sum(monthList) == sum(refMonthList) + 1 , "apply leap day to 2004" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2000_DI)
+    call assert_true( sum(monthList) == sum(refMonthList) + 1 , "apply leap day to 2000" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 1996_DI)
+    call assert_true( sum(monthList) == sum(refMonthList) + 1 , "apply leap day to 1996" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2001_DI)
+    call assert_true( sum(monthList) == sum(refMonthList) , "do not apply leap day to 2001" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 1600_DI)
+    call assert_true( sum(monthList) == sum(refMonthList) + 1 , "apply leap day to 1600" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 1900_DI)
+    call assert_true( sum(monthList) == sum(refMonthList) , "do not apply leap day to 1900" )
+  end subroutine test_applyLeapDay
+
+  subroutine test_calcDayOfYear
+    use types_mod
+    integer(kind=DI) :: monthList(12)
+
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2004_DI)
+    call assert_true( calcDayOfYear(monthList, 1_DI, 1_DI) == 0_DI , "Jan 1 2004 is day 0" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2004_DI)
+    call assert_true( calcDayOfYear(monthList, 12_DI, 31_DI) == 365_DI , "Dec 31 1 2004 is day 365" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2004_DI)
+    call assert_true( calcDayOfYear(monthList, 6_DI, 12_DI) == 163_DI , "June 12 2004 is day 163" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2001_DI)
+    call assert_true( calcDayOfYear(monthList, 1_DI, 1_DI) == 0_DI , "Jan 1 2001 is day 0" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2001_DI)
+    call assert_true( calcDayOfYear(monthList, 12_DI, 31_DI) == 364_DI , "Dec 31 1 2001 is day 364" )
+    monthList = refMonthList
+    call applyLeapDay( monthList, 2001_DI)
+    call assert_true( calcDayOfYear(monthList, 6_DI, 12_DI) == 162_DI , "June 12 2001 is day 162" )
+  end subroutine test_calcDayOfYear
+
+  subroutine test_calcInitialDateParameters
+    use types_mod
+
+    startDay = 1_DI
+    startMonth = 1_DI
+    startYear = 2004_DI
+    call calcInitialDateParameters()
+    call assert_true( startDayOfYear == 0_DI , "Initial Jan 1 2004 is day 0")
+
+    startDay = 31_DI
+    startMonth = 12_DI
+    startYear = 2004_DI
+    call calcInitialDateParameters()
+    call assert_true( startDayOfYear == 365_DI , "Initial Dec 31 2004 is day 365")
+
+    startDay = 12_DI
+    startMonth = 6_DI
+    startYear = 2004_DI
+    call calcInitialDateParameters()
+    call assert_true( startDayOfYear == 163 , "Initial June 12 2004 is day 163")
+
+    startDay = 1_DI
+    startMonth = 1_DI
+    startYear = 2001_DI
+    call calcInitialDateParameters()
+    call assert_true( startDayOfYear == 0_DI , "Initial Jan 1 2001 is day 0")
+
+    startDay = 31_DI
+    startMonth = 12_DI
+    startYear = 2001_DI
+    call calcInitialDateParameters()
+    call assert_true( startDayOfYear == 364_DI , "Initial Dec 31 2001 is day 364")
+
+    startDay = 12_DI
+    startMonth = 6_DI
+    startYear = 2001_DI
+    call calcInitialDateParameters()
+    call assert_true( startDayOfYear == 162 , "Initial June 12 2001 is day 162")
+  end subroutine test_calcInitialDateParameters
+
+  subroutine test_calcCurrentDateParameters
+    use types_mod
+
+    ! early in the day
+    startDay = 1_DI
+    startMonth = 1_DI
+    startYear = 2004_DI
+    call calcCurrentDateParameters( 10.0_DP )
+    call assert_true( currentDayOfYear == 0_DI , "Current day of year, starting at Jan 1 2004, with 10 seconds, is day 0")
+    call assert_true( currentMonth == 1_DI , "Current month, starting at Jan 1 2004, with 10 seconds, is month 1")
+    call assert_true( currentDayOfMonth == 1_DI , "Current day of month, starting at Jan 1 2004, with 10 seconds, is day 1")
+
+    ! last second in the same day
+    call calcCurrentDateParameters( 86399.0_DP )
+    call assert_true( currentDayOfYear == 0_DI , "Current day of year, starting at Jan 1 2004, with 86399 seconds, is day 0")
+    call assert_true( currentMonth == 1_DI , "Current month, starting at Jan 1 2004, with 86399 seconds, is month 1")
+    call assert_true( currentDayOfMonth == 1_DI , "Current day of month, starting at Jan 1 2004, with 86399 seconds, is day 1")
+
+    ! first second in the next day
+    call calcCurrentDateParameters( 86400.0_DP )
+    call assert_true( currentDayOfYear == 1_DI , "Current day of year, starting at Jan 1 2004, with 86400 seconds, is day 1")
+    call assert_true( currentMonth == 1_DI , "Current month, starting at Jan 1 2004, with 86400 seconds, is month 1")
+    call assert_true( currentDayOfMonth == 2_DI , "Current day of month, starting at Jan 1 2004, with 86400 seconds, is day 2")
+
+    ! last second of a day after leap day
+    startDay = 1_DI
+    startMonth = 1_DI
+    startYear = 2004_DI
+    call calcCurrentDateParameters( 163.0_DP * 86400.0_DP-1 )
+    call assert_true( currentDayOfYear == 162_DI , "Current day of year, starting at Jan 1 2004, &
+    &with 163*86400-1 seconds, is day 162")
+    call assert_true( currentMonth == 6_DI , "Current month, starting at Jan 1 2004, with 163*86400-1 seconds, is month 6")
+    call assert_true( currentDayOfMonth == 11_DI , "Current day of month, starting at Jan 1 2004, &
+    &with 163*86400-1 seconds, is day 11")
+
+    ! first second of a day after leap day
+    call calcCurrentDateParameters( 163.0_DP * 86400.0_DP )
+    call assert_true( currentDayOfYear == 163_DI , "Current day of year, starting at Jan 1 2004, &
+    &with 163*86400 seconds, is day 163")
+    call assert_true( currentMonth == 6_DI , "Current month, starting at Jan 1 2004, with 163*86400 seconds, is month 6")
+    call assert_true( currentDayOfMonth == 12_DI , "Current day of month, starting at Jan 1 2004, &
+    &with 163*86400 seconds, is day 12")
+
+    ! last second of a day after non-existent leap day
+    startDay = 1_DI
+    startMonth = 1_DI
+    startYear = 2001_DI
+    call calcCurrentDateParameters( 163.0_DP * 86400.0_DP-1 )
+    call assert_true( currentDayOfYear == 162_DI , "Current day of year, starting at Jan 1 2001, &
+    &with 163*86400-1 seconds, is day 162")
+    call assert_true( currentMonth == 6_DI , "Current month, starting at Jan 1 2001, with 163*86400-1 seconds, is month 6")
+    call assert_true( currentDayOfMonth == 12_DI , "Current day of month, starting at Jan 1 2001, &
+    &with 163*86400-1 seconds, is day 12")
+
+    ! first second of a day after non-existent leap day
+    call calcCurrentDateParameters( 163.0_DP * 86400.0_DP )
+    call assert_true( currentDayOfYear == 163_DI , "Current day of year, starting at Jan 1 2001, &
+    &with 163*86400 seconds, is day 163")
+    call assert_true( currentMonth == 6_DI , "Current month, starting at Jan 1 2001, with 163*86400 seconds, is month 6")
+    call assert_true( currentDayOfMonth == 13_DI , "Current day of month, starting at Jan 1 2001, &
+    &with 163*86400 seconds, is day 13")
+
+    ! last second of a day after leap day with different start day
+    startDay = 12_DI
+    startMonth = 2_DI
+    startYear = 2004_DI
+    call calcCurrentDateParameters( 163.0_DP * 86400.0_DP-1 )
+    call assert_true( currentDayOfYear == 204_DI , "Current day of year, starting at June 12 2004, &
+    &with 163*86400-1 seconds, is day 162")
+    call assert_true( currentMonth == 7_DI , "Current month, starting at June 12 2004, with 163*86400-1 seconds, is month 6")
+    call assert_true( currentDayOfMonth == 23_DI , "Current day of month, starting at June 12 2004, &
+    &with 163*86400-1 seconds, is day 11")
+
+    ! first second of a day after leap day with different start day
+    call calcCurrentDateParameters( 163.0_DP * 86400.0_DP )
+    call assert_true( currentDayOfYear == 205_DI , "Current day of year, starting at Feb 12 2004, &
+    &with 163*86400 seconds, is day 163")
+    call assert_true( currentMonth == 7_DI , "Current month, starting at Feb 12 2004, with 163*86400 seconds, is month 6")
+    call assert_true( currentDayOfMonth == 24_DI , "Current day of month, starting at Feb 12 2004, &
+    &with 163*86400 seconds, is day 12")
+
+    ! last second of a day after non-existent leap day with different start day
+    startDay = 12_DI
+    startMonth = 2_DI
+    startYear = 2001_DI
+    call calcCurrentDateParameters( 163.0_DP * 86400.0_DP-1 )
+    call assert_true( currentDayOfYear == 204_DI , "Current day of year, starting at Feb 12 2001, &
+    &with 163*86400-1 seconds, is day 162")
+    call assert_true( currentMonth == 7_DI , "Current month, starting at Feb 12 2001, with 163*86400-1 seconds, is month 6")
+    call assert_true( currentDayOfMonth == 24_DI , "Current day of month, starting at Feb 12 2001, &
+    &with 163*86400-1 seconds, is day 12")
+
+    ! first second of a day after non-existent leap day with different start day
+    call calcCurrentDateParameters( 163.0_DP * 86400.0_DP )
+    call assert_true( currentDayOfYear == 205_DI , "Current day of year, starting at Feb 12 2001, &
+    &with 163*86400 seconds, is day 163")
+    call assert_true( currentMonth == 7_DI , "Current month, starting at Feb 12 2001, with 163*86400 seconds, is month 6")
+    call assert_true( currentDayOfMonth == 25_DI , "Current day of month, starting at Feb 12 2001, &
+    &with 163*86400 seconds, is day 13")
+
+    startDay = 31_DI
+    startMonth = 12_DI
+    startYear = 2001_DI
+    call calcCurrentDateParameters( 1.0_DP * 86400.0_DP )
+    call assert_true( currentYear == 2002_DI , "Current year, starting at Dec 31 2001, &
+    &with 1*86400 seconds, is 2002")
+    call calcCurrentDateParameters( 1.0_DP * 86400.0_DP-1.0_DP)
+    call assert_true( currentYear == 2001_DI , "Current year, starting at Dec 31 2001, &
+    &with 1*86400-1 seconds, is 2001")
+    call calcCurrentDateParameters( (1.0_DP + 365.0_DP) * 86400.0_DP )
+    call assert_true( currentYear == 2003_DI , "Current year, starting at Dec 31 2001, &
+    &with (1+365)*86400 seconds, is 2003")
+    call calcCurrentDateParameters( (1.0_DP + 365.0_DP) * 86400.0_DP-1.0_DP)
+    call assert_true( currentYear == 2002_DI , "Current year, starting at Dec 31 2001, &
+    &with (1+365)*86400-1 seconds, is 2002")
+    call calcCurrentDateParameters( (1.0_DP + 365.0_DP + 365.0_DP) * 86400.0_DP )
+    call assert_true( currentYear == 2004_DI , "Current year, starting at Dec 31 2001, &
+    &with (1+365+365)*86400 seconds, is 2004")
+    call calcCurrentDateParameters( (1.0_DP + 365.0_DP + 365.0_DP) * 86400.0_DP-1.0_DP)
+    call assert_true( currentYear == 2003_DI , "Current year, starting at Dec 31 2001, &
+    &with (1+365+365)*86400-1 seconds, is 2003")
+    call calcCurrentDateParameters( (1.0_DP + 365.0_DP + 365.0_DP + 366.0_DP) * 86400.0_DP )
+    call assert_true( currentYear == 2005_DI , "Current year, starting at Dec 31 2001, &
+    &with (1+365+365+366)*86400 seconds, is 2005")
+    call calcCurrentDateParameters( (1.0_DP + 365.0_DP + 365.0_DP + 366.0_DP) * 86400.0_DP-1.0_DP)
+    call assert_true( currentYear == 2004_DI , "Current year, starting at Dec 31 2001, &
+    &with (1+365+365+366*86400-1 seconds, is 2004")
+  end subroutine test_calcCurrentDateParameters
+
+end module date_test

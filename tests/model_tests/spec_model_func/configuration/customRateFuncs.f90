d4042c37987bae6ca87a132cc0fdb61cfaa3afb9
Alfred.Mayhew <alfiemayhew@gmail.com>
2024-01-11 16:09:05 -0700

    Added copyright headers to each of the customRateFuncs.f90 files

--- tests/model_tests/spec_model_func/configuration/customRateFuncs.f90
+++ tests/model_tests/spec_model_func/configuration/customRateFuncs.f90
@@ -1,3 +1,18 @@
+! -----------------------------------------------------------------------------
+!
+! Copyright (c) 2009 - 2012 Chris Martin, Kasia Boronska, Jenny Young,
+! Peter Jimack, Mike Pilling
+!
+! Copyright (c) 2017 Sam Cox, Roberto Sommariva
+!
+! Copyright (c) 2023 Alfred Mayhew
+!
+! This file is part of the AtChem2 software package.
+!
+! This file is covered by the MIT license which can be found in the file
+! LICENSE.md at the top level of the AtChem2 distribution.
+!
+! -----------------------------------------------------------------------------
 ! ******************************************************************** !
 ! ATCHEM2 -- MODULE customRateFunctions
 !

40f515c858aca21564b46b90486f85122900a667
willdrysdale <wsd500@york.ac.uk>
2024-08-02 14:23:15 +0100

    update to run with singularity

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,5 @@ RUN /install.sh
 
 LABEL org.opencontainers.image.source=https://github.com/wacl-york/AtChem2
 
-ENTRYPOINT [ "./entrypoint.sh" ]
+ENTRYPOINT [ "/entrypoint.sh" ]
 #ENTRYPOINT [ "/bin/bash" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
+\command cp -rf /AtChem2-1.2.2/ ~/ # make a copy to home to allow for compatibility with singularity 
+\command cp -rf /inout/* ~/AtChem2-1.2.2/
 
-\command cp -rf /inout/* /AtChem2-1.2.2/
-
-cd AtChem2-1.2.2
+cd ~/AtChem2-1.2.2/
 
 ./build/build_atchem2.sh ./mcm/$1
 echo $1
 
 ./atchem2
 
-cp -r /AtChem2-1.2.2/model/output /inout/model/output/
+cp -r ~/AtChem2-1.2.2/model/output /inout/model/output/
